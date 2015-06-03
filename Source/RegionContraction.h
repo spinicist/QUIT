01c2b992af4140835b617dea909ee171cb6fc7c0
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-06-03 14:48:00 +0100

    Added despot2fm and RegionContraction.

---
+++ Source/RegionContraction.h
@@ -0,0 +1,278 @@
+/*
+ *  RegionContraction.h
+ *
+ *  Created by Tobias Wood on 17/08/2012.
+ *  Copyright (c) 2012-2013 Tobias Wood.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef DESPOT_RegionContraction_h
+#define DESPOT_RegionContraction_h
+
+#include <vector>
+#include <random>
+#include <iostream>
+#include <atomic>
+
+#include <Eigen/Dense>
+
+#include "Util.h"
+
+using namespace std;
+using namespace Eigen;
+
+typedef Array<bool, Dynamic, 1> ArrayXb;
+
+vector<size_t> index_partial_sort(const Ref<ArrayXd> &x, ArrayXd::Index N);
+vector<size_t> index_partial_sort(const Ref<ArrayXd> &x, ArrayXd::Index N)
+{
+	eigen_assert(x.size() >= N);
+    vector<size_t> allIndices(x.size()), indices(N);
+    for(size_t i = 0; i < allIndices.size(); i++) {
+		allIndices[i] = i;
+    }
+	partial_sort(allIndices.begin(), allIndices.begin() + N, allIndices.end(),
+	             [&x](size_t i1, size_t i2) { return x[i1] < x[i2]; });
+	for (ArrayXd::Index i = 0; i < N; i++) {
+		indices[i] = allIndices[i];
+	}
+    return indices;
+}
+
+enum class RCStatus {
+	NotStarted = -1,
+	Converged, IterationLimit, ErrorInvalid, ErrorResidual
+};
+
+ostream& operator<<(ostream &os, const RCStatus &s) {
+	switch (s) {
+		case RCStatus::NotStarted: os << "Not Started"; break;
+		case RCStatus::Converged: os << "Converged"; break;
+		case RCStatus::IterationLimit: os << "Reached iteration limit"; break;
+		case RCStatus::ErrorInvalid: os << "Could not generate valid sample"; break;
+		case RCStatus::ErrorResidual: os << "Infinite residual found"; break;
+	}
+	return os;
+}
+
+template <typename Functor_t>
+class RegionContraction {
+	private:
+		Functor_t &m_f;
+		mt19937_64 m_rng;
+		ArrayXXd m_startBounds, m_currentBounds;
+		ArrayXd m_weights, m_residuals, m_threshes;
+		size_t m_nS, m_nR, m_maxContractions, m_contractions;
+		double m_expand, m_SoS;
+		RCStatus m_status;
+		bool m_gaussian, m_debug;
+	
+	public:
+		RegionContraction(Functor_t &f,
+						  const Ref<ArrayXXd> &startBounds, const ArrayXd &weights, const ArrayXd &thresh,
+						  const int nS = 5000, const int nR = 50, const int maxContractions = 10,
+						  const double expand = 0., const bool gauss = false, const bool debug = false, const int seed = -1) :
+				m_f(f), m_startBounds(startBounds), m_currentBounds(startBounds),
+				m_nS(nS), m_nR(nR), m_maxContractions(maxContractions),
+				m_threshes(thresh), m_expand(expand), m_residuals(f.values()), m_contractions(0),
+				m_status(RCStatus::NotStarted), m_weights(weights), m_gaussian(gauss), m_debug(debug)
+		{
+			eigen_assert(f.inputs() == startBounds.rows());
+			eigen_assert(startBounds.cols() == 2);
+			eigen_assert(weights.rows() == f.values());
+			eigen_assert(thresh.rows() == f.inputs());
+			eigen_assert((thresh >= 0.).all() && (thresh <= 1.).all());
+
+			if (seed < 0) {
+				m_rng = mt19937_64(QUITK::RandomSeed());
+			} else {
+				m_rng = mt19937_64(seed);
+			}
+		}
+		
+		const ArrayXXd &startBounds() const { return m_startBounds; }
+		void setBounds(const Ref<ArrayXXd> &b) {
+			eigen_assert(m_f.inputs() == b.rows());
+			eigen_assert(b.cols() == 2);
+			m_startBounds = b;
+		}
+		const ArrayXd &weights() const { return m_weights; }
+		void setWeights(const Ref<ArrayXd> &w) {
+			eigen_assert(w.rows() == m_f.values());
+			m_weights = w;
+		}
+		const ArrayXb &thresholds() const { return m_threshes; }
+		void setThresholds(const Ref<ArrayXd> &t) {
+			eigen_assert(t.rows() == m_f.inputs());
+			eigen_assert((t >= 0.).all() && (t <= 1.).all());
+			m_threshes = t;
+		}
+		const ArrayXd  &residuals() const { return m_residuals; }
+		const size_t   contractions() const { return m_contractions; }
+		RCStatus       status() const { return m_status; }
+		const ArrayXXd &currentBounds() const { return m_currentBounds; }
+		const double   SoS() const { return m_SoS; }
+		const ArrayXd  startWidth() const { return m_startBounds.col(1) - m_startBounds.col(0); }
+		const ArrayXd  width() const { return m_currentBounds.col(1) - m_currentBounds.col(0); }
+		const ArrayXd  midPoint() const { return (m_currentBounds.rowwise().sum() / 2.); }
+		
+		void optimise(Ref<ArrayXd> params) {
+			static atomic<bool> finiteWarning(false);
+			static atomic<bool> constraintWarning(false);
+			static atomic<bool> boundsWarning(false);
+			mutex warn_mtx;
+
+			eigen_assert(m_f.inputs() == params.size());
+			int nP = static_cast<int>(params.size());
+			ArrayXXd samples(m_f.inputs(), m_nS);
+			ArrayXXd retained(m_f.inputs(), m_nR);
+			ArrayXXd residuals(m_f.values(), m_nS);
+			ArrayXXd retainedRes(m_f.values(), m_nR);
+			ArrayXd gauss_mu(m_f.inputs()), gauss_sigma(m_f.inputs());
+			vector<size_t> indices(m_nR);
+			m_currentBounds = m_startBounds;
+			m_residuals.setZero();
+
+			if ((m_startBounds != m_startBounds).any() || (m_startBounds >= numeric_limits<double>::infinity()).any()) {
+				warn_mtx.lock();
+				if (!boundsWarning) {
+					boundsWarning = true;
+					cerr << "Warning: Starting boundaries do not make sense." << endl;
+					cerr << "Bounds were: " << m_startBounds.transpose() << endl;
+					cerr << "This warning will only be printed once." << endl;
+				}
+				warn_mtx.unlock();
+				params.setZero();
+				m_status = RCStatus::ErrorInvalid;
+
+				return;
+			}
+
+			if (m_debug) {
+				cout << endl;
+				cout << "Start Boundaries: " << endl << m_startBounds.transpose() << endl;
+				cout << "Weights:        " << m_weights.transpose() << endl;
+			}
+			
+			uniform_real_distribution<double> uniform(0., 1.);
+			m_status = RCStatus::IterationLimit;
+			for (m_contractions = 0; m_contractions < m_maxContractions; m_contractions++) {
+				size_t startSample = 0;
+				if (m_contractions > 0) {
+					// Keep the retained samples to prevent boundary contracting too fast
+					for (size_t s = 0; s < m_nR; s++) {
+						samples.col(s) = retained.col(s);
+						residuals.col(s) = retainedRes.col(s);
+					}
+					startSample = m_nR;
+				}
+				for (size_t s = startSample; s < m_nS; s++) {
+					ArrayXd tempSample(nP);
+					size_t nTries = 0;
+					do {
+						if (!m_gaussian || (m_contractions == 0)) {
+							for (int p = 0; p < nP; p++) {
+								tempSample(p) = uniform(m_rng);
+							}
+							tempSample *= width();
+							tempSample += m_currentBounds.col(0);
+						} else {
+							for (int p = 0; p < nP; p++) {
+								normal_distribution<double> gauss(gauss_mu(p), gauss_sigma(p));
+								tempSample(p) = gauss(m_rng);
+								if (tempSample(p) < m_currentBounds(p, 0))
+									tempSample(p) = m_currentBounds(p, 0);
+								if (tempSample(p) > m_currentBounds(p, 1))
+									tempSample(p) = m_currentBounds(p, 1);
+							}
+						}
+						nTries++;
+						if (nTries > 100) {
+							warn_mtx.lock();
+							if (!constraintWarning) {
+								constraintWarning = true;
+								cerr << "Warning: Cannot fulfill sample constraints after " << to_string(nTries) << " attempts, giving up." << endl;
+								cerr << "Last attempt was: " << tempSample.transpose() << endl;
+								cerr << "This warning will only be printed once." << endl;
+							}
+							warn_mtx.unlock();
+							params.setZero();
+							m_status = RCStatus::ErrorInvalid;
+							return;
+						}
+					} while (!m_f.constraint(tempSample));
+					
+					m_f(tempSample, residuals.col(s));
+					if (!isfinite(residuals.col(s).square().sum())) {
+						warn_mtx.lock();
+						if (!finiteWarning) {
+							finiteWarning = true;
+							cout << "Warning: Non-finite residual found!" << endl
+								 << "Result may be meaningless. This warning will only be printed once." << endl;
+							cout << "Parameters were " << tempSample.transpose() << endl;
+						}
+						warn_mtx.unlock();
+						params = retained.col(0);
+						m_residuals.setConstant(numeric_limits<double>::infinity());
+						m_status = RCStatus::ErrorResidual;
+						return;
+					}
+					samples.col(s) = tempSample;
+				}
+				ArrayXd toSort = (residuals.colwise() * m_weights).square().colwise().sum();
+				indices = index_partial_sort(toSort, m_nR);
+				ArrayXd previousBest = retained.col(0);
+				for (size_t i = 0; i < m_nR; i++) {
+					retained.col(i) = samples.col(indices[i]);
+					retainedRes.col(i) = residuals.col(indices[i]);
+				}
+				// Find the min and max for each parameter in the top nR samples
+				m_currentBounds.col(0) = retained.rowwise().minCoeff();
+				m_currentBounds.col(1) = retained.rowwise().maxCoeff();
+				if (m_gaussian) {
+					gauss_mu = retained.rowwise().mean();
+					gauss_sigma = ((retained.colwise() - gauss_mu).square().rowwise().sum() / m_f.inputs()).sqrt();
+				}
+				// Terminate if all the desired parameters have converged
+				if (m_debug) {
+					cout << "Best sample:    " << retained.col(0).transpose() << endl;
+					cout << "Best residual:  " << retainedRes.col(0).transpose() << endl;
+					cout << "Best SoS:       " << retainedRes.col(0).square().sum() << endl;
+					cout << "Current bounds: " << endl << m_currentBounds.transpose() << endl;
+					cout << "Current width:  " << width().transpose() << endl;
+					cout << "Current thresh: " << (m_threshes * startWidth()).transpose() << endl;
+					cout << "Width < Thresh: " << (width() <= (m_threshes * startWidth())).transpose() << endl;
+					cout << "Converged:      " << (width() <= (m_threshes * startWidth())).all() << endl;
+					if (m_gaussian) {
+						cout << "Gaussian mu:    " << gauss_mu.transpose() << endl;
+						cout << "Gaussian sigma: "<<  gauss_sigma.transpose() << endl;
+					}
+				}
+				if (((width() <= (m_threshes * startWidth())).all()) ||
+				    (previousBest == retained.col(0)).all()) {
+					m_status = RCStatus::Converged;
+					m_contractions++; // Just to give an accurate contraction count.
+					break;
+				}
+				
+				// Expand the boundaries back out in case we just missed a minima,
+				// but don't go past initial boundaries
+				ArrayXd tempW = width(); // Because altering .col(0) will change width
+				m_currentBounds.col(0) = (m_currentBounds.col(0) - tempW * m_expand).max(m_startBounds.col(0));
+				m_currentBounds.col(1) = (m_currentBounds.col(1) + tempW * m_expand).min(m_startBounds.col(1));
+			}
+			// Return the best evaluated solution so far
+			params = retained.col(0);
+			m_residuals = retainedRes.col(0);
+			m_SoS = m_residuals.square().sum();
+			if (m_debug) {
+				cout << "Finished, contractions = " << m_contractions << endl;
+			}
+		}
+};
+
+#endif
---
+++ Source/despot2fm.cpp
@@ -0,0 +1,304 @@
+/*
+ *  mcDESPOT_main.cpp
+ *
+ *  Created by Tobias Wood on 2013/08/12.
+ *  Copyright (c) 2013 Tobias Wood.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <time.h>
+#include <getopt.h>
+#include <iostream>
+#include <atomic>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/LevenbergMarquardt>
+#include <unsupported/Eigen/NumericalDiff>
+
+#include "Util.h"
+#include "Filters/ApplyAlgorithmFilter.h"
+#include "Filters/ReorderVectorFilter.h"
+#include "Model.h"
+#include "Sequence.h"
+#include "RegionContraction.h"
+
+using namespace std;
+using namespace Eigen;
+using namespace QUITK;
+
+//******************************************************************************
+// Arguments / Usage
+//******************************************************************************
+const string usage {
+"Usage is: despot2-fm [options] T1_map ssfp_file\n\
+\
+Options:\n\
+	--help, -h        : Print this message\n\
+	--verbose, -v     : Print slice processing times\n\
+	--no-prompt, -n   : Suppress input prompts\n\
+	--mask, -m file   : Mask input with specified file\n\
+	--out, -o path    : Add a prefix to the output filenames\n\
+	--f0, -f SYM      : Fit symmetric f0 map (default)\n\
+	         ASYM     : Fit asymmetric f0 map\n\
+	         file     : Use f0 Map file (in Hertz)\n\
+	--B1, -b file     : B1 Map file (ratio)\n\
+	--start, -s N     : Start processing from slice N\n\
+	--stop, -p  N     : Stop processing at slice N\n\
+	--scale, -S 0     : Normalise signals to mean (default)\n\
+	            1     : Fit a scaling factor/proton density\n\
+	--flip, -F        : Data order is phase, then flip-angle (default opposite)\n\
+	--sequences, -M s : Use simple sequences (default)\n\
+	            f     : Use finite pulse length correction\n\
+	--contract, -c n  : Read contraction settings from stdin (Will prompt)\n\
+	--resids, -r      : Write out per flip-angle residuals\n\
+	--threads, -T N   : Use N threads (default=hardware limit)\n"
+};
+/* --complex, -x     : Fit to complex data\n\ */
+
+static auto tesla = FieldStrength::Three;
+static size_t start_slice = 0, stop_slice = numeric_limits<size_t>::max();
+static int verbose = false, prompt = true, all_residuals = false,
+           fitFinite = false, fitComplex = false, flipData = false,
+           samples = 2000, retain = 20, contract = 10,
+           seed = -1;
+static double expand = 0.;
+static string outPrefix;
+static struct option long_opts[] = {
+	{"help", no_argument, 0, 'h'},
+	{"verbose", no_argument, 0, 'v'},
+	{"no-prompt", no_argument, 0, 'n'},
+	{"mask", required_argument, 0, 'm'},
+	{"out", required_argument, 0, 'o'},
+	{"f0", required_argument, 0, 'f'},
+	{"B1", required_argument, 0, 'b'},
+	{"start", required_argument, 0, 's'},
+	{"stop", required_argument, 0, 'p'},
+	{"scale", required_argument, 0, 'S'},
+	{"flip", required_argument, 0, 'F'},
+	{"threads", required_argument, 0, 'T'},
+	{"sequences", no_argument, 0, 'M'},
+	/*{"complex", no_argument, 0, 'x'},*/
+	{"contract", no_argument, 0, 'c'},
+	{"resids", no_argument, 0, 'r'},
+	{0, 0, 0, 0}
+};
+static const char* short_opts = "hvnm:o:f:b:s:p:S:FT:M:xcrd:";
+
+//******************************************************************************
+// T2 Only Functor
+//******************************************************************************
+class FMFunctor : public DenseFunctor<double> {
+	public:
+		const shared_ptr<SequenceBase> m_sequence;
+		shared_ptr<SCD> m_model;
+		ArrayXd m_data;
+		const double m_T1, m_B1;
+
+		FMFunctor(const shared_ptr<SCD> m, const double T1, const shared_ptr<SequenceBase> s, const ArrayXd &d, const double B1) :
+			DenseFunctor<double>(3, s->size()),
+			m_model(m), m_sequence(s), m_data(d),
+			m_T1(T1), m_B1(B1)
+		{
+			assert(static_cast<size_t>(m_data.rows()) == values());
+		}
+
+		const bool constraint(const VectorXd &params) const {
+			Array4d fullparams;
+			fullparams << params(0), m_T1, params(1), params(2);
+			return m_model->ValidParameters(fullparams);
+		}
+
+		int operator()(const Ref<VectorXd> &params, Ref<ArrayXd> diffs) const {
+			eigen_assert(diffs.size() == values());
+
+			ArrayXd fullparams(5);
+			fullparams << params(0), m_T1, params(1), params(2), m_B1;
+			ArrayXcd s = m_sequence->signal(m_model, fullparams);
+			diffs = s.abs() - m_data;
+			return 0;
+		}
+};
+
+class FMAlgo : public Algorithm<double> {
+	private:
+		size_t m_samples = 2000, m_retain = 20, m_contractions = 10;
+		Array2d m_f0Bounds = Array2d::Zero();
+		const shared_ptr<SCD> m_model = make_shared<SCD>();
+
+	public:
+		void setSamples(size_t s) { m_samples = s; }
+		void setRetain(size_t r) { m_retain = r; }
+		void setContractions(size_t c) { m_contractions = c; }
+		void setScaling(Model::Scale s) { m_model->setScaling(s); }
+		void setf0Bounds(Array2d b) { m_f0Bounds = b; }
+
+		size_t numConsts() const override { return 2; }
+		size_t numOutputs() const override { return 3; }
+
+		virtual VectorXd defaultConsts() {
+			// T1 & B1
+			VectorXd def = VectorXd::Ones(2);
+			return def;
+		}
+
+		virtual void apply(const shared_ptr<SequenceBase> sequence,
+		                   const VectorXd &data,
+		                   const VectorXd &inputs,
+		                   VectorXd &outputs,
+		                   ArrayXd &resids) const override
+		{
+			ArrayXd thresh(3); thresh.setConstant(0.05);
+			ArrayXd weights(sequence->size()); weights.setOnes();
+			ArrayXXd bounds = ArrayXXd::Zero(3, 2);
+			double T1 = inputs[0];
+			double B1 = inputs[1];
+			if (m_model->scaling() == Model::Scale::None) {
+				bounds(0, 0) = 0.;
+				bounds(0, 1) = data.array().abs().maxCoeff() * 25;
+			} else {
+				bounds.row(0).setConstant(1.);
+			}
+			bounds(1,0) = 0.001;
+			bounds(1,1) = T1;
+			bounds.row(2) = m_f0Bounds;
+			//cout << "T1 " << T1 << " B1 " << B1 << " inputs " << inputs.transpose() << endl;
+			//cout << bounds << endl;
+			FMFunctor func(m_model, T1, sequence, data, B1);
+			RegionContraction<FMFunctor> rc(func, bounds, weights, thresh,
+			                                m_samples, m_retain, m_contractions, expand, false, false);
+			rc.optimise(outputs);
+			resids = rc.residuals();
+		}
+};
+
+//******************************************************************************
+// Main
+//******************************************************************************
+int main(int argc, char **argv) {
+	Eigen::initParallel();
+	ReadFloatImage::Pointer mask, B1, f0 = ITK_NULLPTR;
+	shared_ptr<FMAlgo> fm = make_shared<FMAlgo>();
+
+	int indexptr = 0, c;
+	while ((c = getopt_long(argc, argv, short_opts, long_opts, &indexptr)) != -1) {
+		switch (c) {
+			case 'v': verbose = true; break;
+			case 'n': prompt = false; break;
+			case 'm':
+				if (verbose) cout << "Reading mask file " << optarg << endl;
+				mask = ReadFloatImage::New();
+				mask->SetFileName(optarg);
+				break;
+			case 'o':
+				outPrefix = optarg;
+				if (verbose) cout << "Output prefix will be: " << outPrefix << endl;
+				break;
+			case 'f':
+				if (verbose) cout << "Reading f0 file: " << optarg << endl;
+				f0 = ReadFloatImage::New();
+				f0->SetFileName(optarg);
+				break;
+			case 'b':
+				if (verbose) cout << "Reading B1 file: " << optarg << endl;
+				B1 = ReadFloatImage::New();
+				B1->SetFileName(optarg);
+				break;
+			/*case 's': start_slice = atoi(optarg); break;
+			case 'p': stop_slice = atoi(optarg); break;*/
+			case 'S':
+				switch (atoi(optarg)) {
+					case 0 : fm->setScaling(Model::Scale::ToMean); break;
+					case 1 : fm->setScaling(Model::Scale::None); break;
+					default:
+						cout << "Invalid scaling mode: " + to_string(atoi(optarg)) << endl;
+						return EXIT_FAILURE;
+						break;
+				} break;
+			case 'F':
+				flipData = true;
+				break;
+			case 'T':
+				itk::MultiThreader::SetGlobalMaximumNumberOfThreads(atoi(optarg));
+				break;
+			case 'd':
+				seed = atoi(optarg);
+				break;
+			case 'M':
+				switch (*optarg) {
+					case 's': fitFinite = false; cout << "Simple sequences selected." << endl; break;
+					case 'f': fitFinite = true; cout << "Finite pulse correction selected." << endl; break;
+					default:
+						cout << "Unknown sequences type " << *optarg << endl;
+						return EXIT_FAILURE;
+						break;
+				}
+				break;
+			case 'c':
+				cout << "Enter max number of contractions: " << flush; cin >> contract;
+				cout << "Enter number of samples per contraction: " << flush; cin >> samples;
+				cout << "Enter number of samples to retain: " << flush; cin >> retain;
+				cout << "Enter fraction to expand region by: " << flush; cin >> expand;
+				break;
+			case 'r': all_residuals = true; break;
+			case '?': // getopt will print an error message
+			case 'h':
+			default:
+				cout << usage << endl;
+				return EXIT_SUCCESS;
+				break;
+		}
+	}
+	if ((argc - optind) != 2) {
+		cout << "Wrong number of arguments. Need a T1 map and one SSFP file." << endl;
+		return EXIT_FAILURE;
+	}
+	shared_ptr<SSFPSimple> ssfpSequence;
+	if (fitFinite) {
+		ssfpSequence = make_shared<SSFPFinite>(prompt);
+	} else {
+		ssfpSequence = make_shared<SSFPSimple>(prompt);
+	}
+	if (verbose) cout << *ssfpSequence << endl;
+
+	if (verbose) cout << "Reading T1 Map from: " << argv[optind] << endl;
+	auto T1File = ReadFloatImage::New();
+	T1File->SetFileName(argv[optind++]);
+
+	if (verbose) cout << "Opening SSFP file: " << argv[optind] << endl;
+	auto ssfpFile = ReadFloatTimeseries::New();
+	ssfpFile->SetFileName(argv[optind++]);
+	auto ssfpData = FloatTimeseriesToVector::New();
+	ssfpData->SetInput(ssfpFile->GetOutput());
+	auto reorderFlip = itk::ReorderVectorFilter<FloatVectorImage>::New();
+	if (flipData) {
+		reorderFlip->SetStride(ssfpSequence->phases());
+	}
+	reorderFlip->SetInput(ssfpData->GetOutput());
+
+	auto apply = itk::ApplyAlgorithmFilter<float, FMAlgo>::New();
+	apply->SetSequence(ssfpSequence);
+	fm->setf0Bounds(ssfpSequence->bandwidth());
+	apply->SetAlgorithm(fm);
+	apply->Setup();
+	apply->SetDataInput(0, reorderFlip->GetOutput());
+	apply->SetConstInput(0, T1File->GetOutput());
+	if (B1)
+		apply->SetConstInput(1, B1->GetOutput());
+	if (mask)
+		apply->SetMask(mask->GetOutput());
+
+	time_t startTime;
+	if (verbose) startTime = printStartTime();
+	apply->Update();
+	printElapsedTime(startTime);
+
+	outPrefix = outPrefix + "FM_";
+	writeResult(apply->GetOutput(0), outPrefix + "PD.nii");
+	writeResult(apply->GetOutput(1), outPrefix + "T2.nii");
+	writeResult(apply->GetOutput(2), outPrefix + "f0.nii");
+	writeResiduals(apply->GetResidOutput(), outPrefix, all_residuals);
+	return EXIT_SUCCESS;
+}
