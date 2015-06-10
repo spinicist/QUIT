102f9ea49be28f2974c80576d3730a99fd19c9c9
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-06-10 13:05:56 +0100

    Implemented Sam Hurley's suggest 2-step fitting process.

--- Source/qidespot2fm.cpp
+++ Source/qidespot2fm.cpp
@@ -41,6 +41,8 @@ Options:\n\
 	--mask, -m file   : Mask input with specified file\n\
 	--out, -o path    : Add a prefix to the output filenames\n\
 	--B1, -b file     : B1 Map file (ratio)\n\
+	--algo, -a l      : Use 2-step LM algorithm (default)\n\
+	           s      : Use Stochastic Region Contraction\n\
 	--start, -s N     : Start processing from slice N\n\
 	--stop, -p  N     : Stop processing at slice N\n\
 	--scale, -S 0     : Normalise signals to mean (default)\n\
@@ -48,7 +50,6 @@ Options:\n\
 	--flip, -F        : Data order is phase, then flip-angle (default opposite)\n\
 	--sequences, -M s : Use simple sequences (default)\n\
 	            f     : Use finite pulse length correction\n\
-	--contract, -c n  : Read contraction settings from stdin (Will prompt)\n\
 	--resids, -r      : Write out per flip-angle residuals\n\
 	--threads, -T N   : Use N threads (default=hardware limit)\n"
 };
@@ -61,105 +62,175 @@ static struct option long_opts[] = {
 	{"mask", required_argument, 0, 'm'},
 	{"out", required_argument, 0, 'o'},
 	{"B1", required_argument, 0, 'b'},
+	{"algo", required_argument, 0, 'a'},
 	{"start", required_argument, 0, 's'},
 	{"stop", required_argument, 0, 'p'},
 	{"scale", required_argument, 0, 'S'},
 	{"flip", required_argument, 0, 'F'},
 	{"threads", required_argument, 0, 'T'},
 	{"sequences", no_argument, 0, 'M'},
-	{"contract", no_argument, 0, 'c'},
 	{"resids", no_argument, 0, 'r'},
 	{0, 0, 0, 0}
 };
-static const char* short_opts = "hvnm:o:b:s:p:S:FT:M:crd:";
+static const char* short_opts = "hvnm:o:b:a:s:p:S:FT:M:rd:";
 
 class FMFunctor : public DenseFunctor<double> {
-	public:
-		const shared_ptr<SequenceBase> m_sequence;
-		shared_ptr<SCD> m_model;
-		ArrayXd m_data;
-		const double m_T1, m_B1;
+public:
+	const shared_ptr<SequenceBase> m_sequence;
+	shared_ptr<SCD> m_model;
+	ArrayXd m_data;
+	const double m_T1, m_B1;
 
-		FMFunctor(const shared_ptr<SCD> m, const double T1, const shared_ptr<SequenceBase> s, const ArrayXd &d, const double B1) :
-			DenseFunctor<double>(3, s->size()),
-			m_model(m), m_sequence(s), m_data(d),
-			m_T1(T1), m_B1(B1)
-		{
-			assert(static_cast<size_t>(m_data.rows()) == values());
-		}
+	FMFunctor(const shared_ptr<SCD> m, const shared_ptr<SequenceBase> s, const ArrayXd &d, const double T1, const double B1) :
+		DenseFunctor<double>(3, s->size()),
+		m_model(m), m_sequence(s), m_data(d),
+		m_T1(T1), m_B1(B1)
+	{
+		assert(static_cast<size_t>(m_data.rows()) == values());
+	}
 
-		const bool constraint(const VectorXd &params) const {
-			Array4d fullparams;
-			fullparams << params(0), m_T1, params(1), params(2);
-			return m_model->ValidParameters(fullparams);
-		}
+	const bool constraint(const VectorXd &params) const {
+		Array4d fullparams;
+		fullparams << params(0), m_T1, params(1), params(2);
+		return m_model->ValidParameters(fullparams);
+	}
 
-		int operator()(const Ref<VectorXd> &params, Ref<ArrayXd> diffs) const {
-			eigen_assert(diffs.size() == values());
+	int operator()(const Ref<VectorXd> &params, Ref<ArrayXd> diffs) const {
+		eigen_assert(diffs.size() == values());
 
-			ArrayXd fullparams(5);
-			fullparams << params(0), m_T1, params(1), params(2), m_B1;
-			ArrayXcd s = m_sequence->signal(m_model, fullparams);
-			diffs = s.abs() - m_data;
-			return 0;
-		}
+		ArrayXd fullparams(5);
+		fullparams << params(0), m_T1, params(1), params(2), m_B1;
+		ArrayXcd s = m_sequence->signal(m_model, fullparams);
+		diffs = s.abs() - m_data;
+		return 0;
+	}
+};
+
+class FixT2 : public DenseFunctor<double> {
+public:
+	const shared_ptr<SequenceBase> m_sequence;
+	shared_ptr<SCD> m_model;
+	ArrayXd m_data;
+	const double m_T1, m_T2, m_B1;
+
+	FixT2(const shared_ptr<SCD> m, const shared_ptr<SequenceBase> s, const ArrayXd &d, const double T1, const double T2, const double B1) :
+		DenseFunctor<double>(2, s->size()),
+		m_model(m), m_sequence(s), m_data(d),
+		m_T1(T1), m_T2(T2), m_B1(B1)
+	{
+		assert(static_cast<size_t>(m_data.rows()) == values());
+	}
+
+	int operator()(const Ref<VectorXd> &params, Ref<ArrayXd> diffs) const {
+		eigen_assert(diffs.size() == values());
+
+		ArrayXd fullparams(5);
+		fullparams << params(0), m_T1, m_T2, params(1), m_B1;
+		ArrayXcd s = m_sequence->signal(m_model, fullparams);
+		diffs = s.abs() - m_data;
+		return 0;
+	}
 };
 
 class FMAlgo : public Algorithm<double> {
-	private:
-		size_t m_samples = 2000, m_retain = 20, m_contractions = 10;
-		Array2d m_f0Bounds = Array2d::Zero();
-		const shared_ptr<SCD> m_model = make_shared<SCD>();
-		shared_ptr<SteadyState> m_sequence;
+protected:
+	const shared_ptr<SCD> m_model = make_shared<SCD>();
+	shared_ptr<SSFPSimple> m_sequence;
 
-	public:
-		void setSequence(shared_ptr<SteadyState> s) { m_sequence = s; }
-		void setRCPars(size_t c, size_t s, size_t r) { m_contractions = c; m_samples = s; m_retain = r; }
-		void setScaling(Model::Scale s) { m_model->setScaling(s); }
-		void setf0Bounds(Array2d b) { m_f0Bounds = b; }
+public:
+	void setSequence(shared_ptr<SSFPSimple> s) { m_sequence = s; }
+	void setScaling(Model::Scale s) { m_model->setScaling(s); }
 
-		size_t numInputs() const override  { return m_sequence->count(); }
-		size_t numConsts() const override  { return 2; }
-		size_t numOutputs() const override { return 3; }
-		size_t dataSize() const override   { return m_sequence->size(); }
+	size_t numInputs() const override  { return m_sequence->count(); }
+	size_t numConsts() const override  { return 2; }
+	size_t numOutputs() const override { return 3; }
+	size_t dataSize() const override   { return m_sequence->size(); }
 
-		virtual TArray defaultConsts() {
-			// T1 & B1
-			VectorXd def = VectorXd::Ones(2);
-			return def;
+	virtual TArray defaultConsts() {
+		// T1 & B1
+		VectorXd def = VectorXd::Ones(2);
+		return def;
+	}
+};
+
+class FMLMAlgo : public FMAlgo {
+protected:
+	static const int m_iterations = 15;
+
+public:
+	virtual void apply(const TInput &data, const TArray &inputs,
+					   TArray &outputs, TArray &resids) const override
+	{
+		const double T1 = inputs[0];
+		if (isfinite(T1) && (T1 > 0.001)) {
+			const double B1 = inputs[1];
+			const double T2 = 0.045 * T1; // From a Yarnykh paper
+			FixT2 fixT2(m_model, m_sequence, data, T1, T2, B1);
+			NumericalDiff<FixT2> fixT2Diff(fixT2);
+			LevenbergMarquardt<NumericalDiff<FixT2>> fixT2LM(fixT2Diff);
+			fixT2LM.setMaxfev(m_iterations * (m_sequence->size() + 1));
+			VectorXd fixT2P(2); fixT2P << data.maxCoeff() * 10., 0.; // Initial guess
+			fixT2LM.minimize(fixT2P);
+
+			FMFunctor full(m_model, m_sequence, data, T1, B1);
+			NumericalDiff<FMFunctor> fullDiff(full);
+			LevenbergMarquardt<NumericalDiff<FMFunctor>> fullLM(fullDiff);
+			VectorXd fullP(3); fullP << fixT2P[0], T2, fixT2P[1]; // Now include T2
+			fullLM.minimize(fullP);
+			outputs = fullP;
+			// PD, T1, B1
+			VectorXd pfull(5); pfull << outputs[0], T1, outputs[1], outputs[2], B1; // Now include EVERYTHING to get a residual
+			ArrayXd theory = m_sequence->signal(m_model, pfull).abs();
+			resids = (data - theory);
+		} else {
+			// No point in processing -ve T1
+			outputs.setZero();
+			resids.setZero();
 		}
+	}
+};
 
-		virtual void apply(const TInput &data, const TArray &inputs,
-		                   TArray &outputs, TArray &resids) const override
-		{
-			ArrayXd thresh(3); thresh.setConstant(0.05);
-			ArrayXd weights(m_sequence->size()); weights.setOnes();
-			ArrayXXd bounds = ArrayXXd::Zero(3, 2);
-			double T1 = inputs[0];
-			if (isfinite(T1) && (T1 > 0.001)) {
-				double B1 = inputs[1];
-				if (m_model->scaling() == Model::Scale::None) {
-					bounds(0, 0) = 0.;
-					bounds(0, 1) = data.array().abs().maxCoeff() * 25;
-				} else {
-					bounds.row(0).setConstant(1.);
-				}
-				bounds(1,0) = 0.001;
-				bounds(1,1) = T1;
-				bounds.row(2) = m_f0Bounds;
-				//cout << "T1 " << T1 << " B1 " << B1 << " inputs " << inputs.transpose() << endl;
-				//cout << bounds << endl;
-				FMFunctor func(m_model, T1, m_sequence, data, B1);
-				RegionContraction<FMFunctor> rc(func, bounds, weights, thresh,
-												m_samples, m_retain, m_contractions, 0.02, true, false);
-				rc.optimise(outputs);
-				resids = rc.residuals();
+
+class FMSRCAlgo : public FMAlgo {
+private:
+	size_t m_samples = 2000, m_retain = 20, m_contractions = 10;
+	Array2d m_f0Bounds = Array2d::Zero();
+
+public:
+	void setRCPars(size_t c, size_t s, size_t r) { m_contractions = c; m_samples = s; m_retain = r; }
+	void setf0Bounds(Array2d b) { m_f0Bounds = b; }
+
+	virtual void apply(const TInput &data, const TArray &inputs,
+					   TArray &outputs, TArray &resids) const override
+	{
+		ArrayXd thresh(3); thresh.setConstant(0.05);
+		ArrayXd weights(m_sequence->size()); weights.setOnes();
+		ArrayXXd bounds = ArrayXXd::Zero(3, 2);
+		double T1 = inputs[0];
+		if (isfinite(T1) && (T1 > 0.001)) {
+			double B1 = inputs[1];
+			if (m_model->scaling() == Model::Scale::None) {
+				bounds(0, 0) = 0.;
+				bounds(0, 1) = data.array().abs().maxCoeff() * 25;
 			} else {
-				// No point in processing -ve T1
-				outputs.setZero();
-				resids.setZero();
+				bounds.row(0).setConstant(1.);
 			}
+			bounds(1,0) = 0.001;
+			bounds(1,1) = T1;
+			bounds.row(2) = m_sequence->bandwidth();
+			//cout << "T1 " << T1 << " B1 " << B1 << " inputs " << inputs.transpose() << endl;
+			//cout << bounds << endl;
+			FMFunctor func(m_model, m_sequence, data, T1, B1);
+			RegionContraction<FMFunctor> rc(func, bounds, weights, thresh,
+											m_samples, m_retain, m_contractions, 0.02, true, false);
+			rc.optimise(outputs);
+			resids = rc.residuals();
+		} else {
+			// No point in processing -ve T1
+			outputs.setZero();
+			resids.setZero();
 		}
+	}
 };
 
 //******************************************************************************
@@ -173,12 +244,29 @@ int main(int argc, char **argv) {
 	    fitFinite = false, flipData = false;
 	string outPrefix;
 	QI::ReadImageF::Pointer mask = ITK_NULLPTR, B1 = ITK_NULLPTR;
-	shared_ptr<FMAlgo> fm = make_shared<FMAlgo>();
-	int indexptr = 0, c;
+	shared_ptr<FMAlgo> fm = make_shared<FMLMAlgo>();
+
+	// Do first pass to get the algorithm type, then do everything else
+	int indexptr = 0;
+	char c;
 	while ((c = getopt_long(argc, argv, short_opts, long_opts, &indexptr)) != -1) {
 		switch (c) {
 			case 'v': verbose = true; break;
 			case 'n': prompt = false; break;
+			case 'a':
+			switch (*optarg) {
+				case 'l': fm = make_shared<FMLMAlgo>();   if (verbose) cout << "LLS algorithm selected." << endl; break;
+				case 's': fm = make_shared<FMSRCAlgo>();  if (verbose) cout << "WLLS algorithm selected." << endl; break;
+				default: throw(runtime_error(string("Unknown algorithm type ") + optarg)); break;
+			} break;
+			default: break;
+		}
+	}
+
+	optind = 1;
+	while ((c = getopt_long(argc, argv, short_opts, long_opts, &indexptr)) != -1) {
+		switch (c) {
+			case 'v': case 'n': case 'a': break; // Already handled
 			case 'm':
 				if (verbose) cout << "Reading mask file " << optarg << endl;
 				mask = QI::ReadImageF::New();
@@ -216,12 +304,6 @@ int main(int argc, char **argv) {
 						break;
 				}
 				break;
-			case 'c': {
-				if (prompt) cout << "Enter max contractions/samples per contraction/retained samples/expand fraction: " << flush;
-				ArrayXi in = ArrayXi::Zero(3);
-				QI::ReadArray(cin, in);
-				fm->setRCPars(in[0], in[1], in[2]);
-			} break;
 			case 'r': all_residuals = true; break;
 			case '?': // getopt will print an error message
 			case 'h':
@@ -258,7 +340,6 @@ int main(int argc, char **argv) {
 	}
 	auto apply = itk::ApplyAlgorithmFilter<QI::VectorImageF, FMAlgo>::New();
 	fm->setSequence(ssfpSequence);
-	fm->setf0Bounds(ssfpSequence->bandwidth());
 	apply->SetAlgorithm(fm);
 	apply->SetDataInput(0, ssfpFlip->GetOutput());
 	apply->SetConstInput(0, T1->GetOutput());
