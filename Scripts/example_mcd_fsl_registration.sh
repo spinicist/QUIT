e69781d2035735d7e1ac3d0affca9459b7c35cca
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-06-17 17:54:02 +0100

    Fixed bugs.

--- Scripts/example_mcd_fsl_registration.sh
+++ Scripts/example_mcd_fsl_registration.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 #
 # Example Processing Script For MCDESPOT data
@@ -6,7 +6,7 @@
 #
 
 if [ $# -eq 0 ]; then
-echo << END_USAGE
+cat << END_USAGE
 Usage: $0 file1.nii file2.nii [file3.nii ...]
 
 This script will register all volumes in each file to the first volume in the
@@ -16,12 +16,13 @@ Requires FSL.
 
 This is almost certainly not the optimal way to do the registration, but should
 be regarded as a framework to improve on.
-
 END_USAGE
 exit 1;
 fi
 
-TARGET="target"
+
+export FSLOUTPUTTYPE=NIFTI
+TARGET="target.nii"
 
 #
 # Extract a target volume
@@ -36,16 +37,16 @@ do
 
 	fslsplit $FILE ${FILE%%.nii*}_split -t
 
-	echo "Registering volumes"
-	for VOL in ${FILE%%.nii*}_split*; do
-		flirt -dof 6 -cost mutualinfo -in $VOL -ref $TARGET -out ${VOL%%nii*}_reg
+	for VOL in ${FILE%%.nii*}_split????.*; do
+		echo "Registering $VOL to $TARGET"
+		flirt -dof 6 -cost mutualinfo -in $VOL -ref $TARGET -out ${VOL%%.nii*}_reg
 	done
 
 	echo "Merging into registered file"
-	fslmerge -t ${FILE%%.nii*}_reg ${VOL$$.nii*}_reg*
+	fslmerge -t ${FILE%%.nii*}_reg ${FILE%%.nii*}_split????_reg.*
 
 	echo "Removing temporary files"
 	rm ${FILE%%.nii*}_split*
 	shift # Prep for next iteration
 done
-rm $TARGET
+rm ${TARGET}
