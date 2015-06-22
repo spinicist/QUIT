22d7536a95d63a9dfe7481f4b8c4deeb49035e38
Tobias Wood <tobias.wood@kcl.ac.uk>
2015-06-22 15:08:35 +0100

    Moved definitions into .hxx file.

--- Source/Filters/ReorderVectorFilter.h
+++ Source/Filters/ReorderVectorFilter.h
@@ -30,58 +30,13 @@ private:
 	void operator=(const Self &);  //purposely not implemented
 
 public:
-	virtual void GenerateOutputInformation() override {
-		//std::cout << __PRETTY_FUNCTION__ << std::endl;
-		Superclass::GenerateOutputInformation();
-		m_fullsize = this->GetInput()->GetNumberOfComponentsPerPixel();
-
-		if (m_blocksize == 0)
-			m_blocksize = m_fullsize;
-
-		if ((m_fullsize % m_blocksize) != 0) {
-			throw(std::runtime_error("Fullsize must be an integer multiple of blocksize."));
-		}
-		if ((m_blocksize % m_stride) != 0) {
-			throw(std::runtime_error("Blocksize must be an integer multiple of stride."));
-		}
-		m_blocks = m_fullsize / m_blocksize;
-		//std::cout << "End " << __PRETTY_FUNCTION__ << std::endl;
-	}
+	virtual void GenerateOutputInformation() override;
 
 protected:
 	ReorderVectorFilter() {}
 	~ReorderVectorFilter() {}
 
-	virtual void ThreadedGenerateData(const TRegion &region, ThreadIdType threadId) {
-		//std::cout <<  __PRETTY_FUNCTION__ << std::endl;
-		//std::cout << *this << std::endl;
-		//std::cout << "Thread " << threadId << std::endl;
-		//std::cout << "Requested region: " << std::endl << region << std::endl;
-		ImageRegionConstIterator<TImage> inIt(this->GetInput(), region);
-		ImageRegionIterator<TImage>      outIt(this->GetOutput(), region);
-
-		inIt.GoToBegin();
-		outIt.GoToBegin();
-		while(!inIt.IsAtEnd()) {
-			VariableLengthVector<TPixel> in = inIt.Get();
-			VariableLengthVector<TPixel> out(in.GetNumberOfElements());
-			size_t outIndex = 0;
-			for (size_t b = 0; b < m_fullsize; b += m_blocksize) { // Tracks the start of each block
-				// Within each block, treat the data as matrix that needs transposing
-				for (size_t j = b; j < m_stride; j++) { // Tracks the 'column'
-					for (size_t i = j; i < m_blocksize; i += m_stride) { // Tracks the elements in 'rows'
-						//std::cout << "b " << b << " j " << j << " i " << i << " out " << outIndex << std::endl;
-						out[outIndex] = in[i];
-						outIndex++;
-					}
-				}
-			}
-			outIt.Set(out);
-			++inIt;
-			++outIt;
-		}
-		//std::cout << "End " << __PRETTY_FUNCTION__ << std::endl;
-	}
+	virtual void ThreadedGenerateData(const TRegion &region, ThreadIdType threadId);
 };
 
 } // End namespace itk
