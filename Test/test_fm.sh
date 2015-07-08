25482bf6a05a4e0bd2c4679ee1780257a7e68741
Tobias Wood <tobias@spinicist.org.uk>
2015-07-08 22:50:17 +0100

    Added extra tests as now it is possible to run SRC with complex data.

--- Test/test_fm.sh
+++ Test/test_fm.sh
@@ -7,7 +7,7 @@
 # Tests whether programs run successfully on toy data
 
 source ./test_common.sh
-SILENCE_TESTS="1"
+SILENCE_TESTS="0"
 
 DATADIR="fm"
 rm -rf $DATADIR
@@ -19,16 +19,15 @@ DIMS="10 10 21"
 VOXDIMS="2 2 2"
 $QUITDIR/qinewimage PD.nii -d "$DIMS" -v "$VOXDIMS" -f 1.0
 $QUITDIR/qinewimage T1.nii -d "$DIMS" -v "$VOXDIMS" -f 1.25
-$QUITDIR/qinewimage T2.nii -d "$DIMS" -v "$VOXDIMS" -g "1 0.025 0.125"
-$QUITDIR/qinewimage f0.nii -d "$DIMS" -v "$VOXDIMS" -g "2 -200.0 200.0"
+$QUITDIR/qinewimage T2.nii -d "$DIMS" -v "$VOXDIMS" -g "1 0.0125 0.125"
+$QUITDIR/qinewimage f0.nii -d "$DIMS" -v "$VOXDIMS" -g "2 -250.0 250.0"
 $QUITDIR/qinewimage B1.nii -d "$DIMS" -v "$VOXDIMS" -f 1.0
 
 # Setup parameters
 SSFP_FILE="ssfp.nii"
 SSFP_FLIP="10 15 20 30 40 50 60"
-SSFP_NEG_FLIP="-15 -60"
-SSFP_TR="0.006"
-SSFP_Trf="0.002"
+SSFP_TR="0.005"
+SSFP_Trf="0.0025"
 
 function run_tests() {
 PREFIX="$1"
@@ -50,12 +49,12 @@ $SSFP_FLIP
 $SSFP_PC
 $SSFP_TR
 $SSFP_Trf
-${PREFIX}xF${SSFP_FILE}
+${PREFIX}xf${SSFP_FILE}
 END
 END_SIG
 
 $QUITDIR/qicomplex -x ${PREFIX}x$SSFP_FILE -M ${PREFIX}$SSFP_FILE -P ${PREFIX}p$SSFP_FILE
-$QUITDIR/qicomplex -x ${PREFIX}xF$SSFP_FILE -M ${PREFIX}F$SSFP_FILE -P ${PREFIX}pF$SSFP_FILE
+$QUITDIR/qicomplex -x ${PREFIX}xf$SSFP_FILE -M ${PREFIX}f$SSFP_FILE -P ${PREFIX}pf$SSFP_FILE
 
 echo "$SSFP_FLIP
 $SSFP_PC
@@ -66,19 +65,23 @@ $SSFP_PC
 $SSFP_TR
 $SSFP_Trf" > ${PREFIX}fm_f_in.txt
 
-run_test "FM"    $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}$SSFP_FILE    -o ${PREFIX}       < ${PREFIX}fm_in.txt
-run_test "SRC"   $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}$SSFP_FILE    -as -o${PREFIX}s   < ${PREFIX}fm_in.txt
-run_test "XFM"   $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}x$SSFP_FILE   -ax -o${PREFIX}x  < ${PREFIX}fm_in.txt
-run_test "FM_F"  $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}F${SSFP_FILE} --finite -o${PREFIX}F       < ${PREFIX}fm_f_in.txt
-run_test "SRC_F" $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}F${SSFP_FILE} -as --finite -o${PREFIX}sF   < ${PREFIX}fm_f_in.txt
-run_test "XFM_F" $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}xF${SSFP_FILE} --finite -ax -o${PREFIX}xF < ${PREFIX}fm_f_in.txt
+run_test "LM"    $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}${SSFP_FILE}  -o ${PREFIX}    < ${PREFIX}fm_in.txt
+run_test "XLM"   $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}x${SSFP_FILE} -x -o${PREFIX}x < ${PREFIX}fm_in.txt
+run_test "SRC"   $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}${SSFP_FILE}  -as -o${PREFIX}s     < ${PREFIX}fm_in.txt
+run_test "XSRC"  $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}x${SSFP_FILE} -x -as -o${PREFIX}sx < ${PREFIX}fm_in.txt
+run_test "LMF"   $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}f${SSFP_FILE}  --finite -o${PREFIX}F     < ${PREFIX}fm_f_in.txt
+run_test "XLMF"  $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}xf${SSFP_FILE} --finite -x -o${PREFIX}xf < ${PREFIX}fm_f_in.txt
+run_test "SRCF"  $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}f${SSFP_FILE}  --finite -as -o${PREFIX}sf     < ${PREFIX}fm_f_in.txt
+run_test "XSRCF" $QUITDIR/qidespot2fm -n -v -bB1.nii T1.nii ${PREFIX}xf${SSFP_FILE} --finite -x -as -o${PREFIX}sxf < ${PREFIX}fm_f_in.txt
 
-compare_test "FM"    T2.nii ${PREFIX}FM_T2.nii   0.01
+compare_test "LM"    T2.nii ${PREFIX}FM_T2.nii   0.01
+compare_test "XLM"   T2.nii ${PREFIX}xFM_T2.nii  0.01
 compare_test "SRC"   T2.nii ${PREFIX}sFM_T2.nii  0.01
-compare_test "XFM"   T2.nii ${PREFIX}xFM_T2.nii  0.015
-compare_test "FM_F"  T2.nii ${PREFIX}FFM_T2.nii  0.01
-compare_test "SRC_F" T2.nii ${PREFIX}sFFM_T2.nii 0.01
-compare_test "XFM_F" T2.nii ${PREFIX}xFFM_T2.nii 0.015
+compare_test "XSRC"  T2.nii ${PREFIX}sxFM_T2.nii 0.01
+compare_test "LMF"   T2.nii ${PREFIX}fFM_T2.nii   0.01
+compare_test "XLMF"  T2.nii ${PREFIX}xfFM_T2.nii  0.01
+compare_test "SRCF"  T2.nii ${PREFIX}sfFM_T2.nii  0.01
+compare_test "XSRCF" T2.nii ${PREFIX}sxfFM_T2.nii 0.01
 }
 
 run_tests "2" "0 180"
