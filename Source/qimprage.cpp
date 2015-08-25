3d0d2b100ea18c7003c691712039bde544c2b8ff
Tobias Wood <tobias@spinicist.org.uk>
2015-08-25 15:46:54 +0100

    Added some extra command line options.

--- Source/qimprage.cpp
+++ Source/qimprage.cpp
@@ -21,51 +21,58 @@
 #include "itkBinaryFunctorImageFilter.h"
 #include "itkImageToHistogramFilter.h"
 #include "itkComplexToModulusImageFilter.h"
+#include "itkComplexToRealImageFilter.h"
 #include "itkBinaryThresholdImageFilter.h"
 #include "itkMaskImageFilter.h"
 
 using namespace std;
 
 const string usage{
-"Usage is: qimp2rage [options] input \n\
+"A tool to process MP3-RAGE images\n\
+Usage is: qimp2rage [options] input \n\
 \
-Input must contain two volumes and be complex-valued\n\
+Input must contain three volumes and be complex-valued\n\
+Output will be files with _CXY where X & Y are inversion times\n\
 Options:\n\
-    --verbose, -v   : Print more messages\n\
-    --mask, -m file : Mask input with specified file.\n\
-    --out, -o path  : Add a prefix to the output filenames.\n"
+    --verbose, -v     : Print more messages.\n\
+    --mask, -m file   : Mask input with specified file.\n\
+    --out, -o path    : Add a prefix to the output filenames.\n\
+    --threshold, -t N : Threshold at Nth quantile.\n\
+    --complex, -x     : Output complex contast images.\n\
+    --threads, -T N   : Use a maximum of N threads.\n"
 };
 
 static struct option long_options[] = {
     {"verbose", required_argument, 0, 'v'},
     {"mask",    required_argument, 0, 'm'},
+    {"threshold", required_argument, 0, 't'},
     {"out",     required_argument, 0, 'o'},
+    {"complex", no_argument, 0, 'x'},
     {"threads", required_argument, 0, 'T'},
     {0, 0, 0, 0}
 };
-static const char *short_options = "vm:o:T:h";
+static const char *short_options = "vm:o:t:xT:h";
 
-template<class T> class MP2RAGE {
+template<class T> class MP2Contrast {
 public:
-    MP2RAGE() {}
-    ~MP2RAGE() {}
-    bool operator!=(const MP2RAGE &) const { return false; }
-    bool operator==(const MP2RAGE &other) const { return !(*this != other); }
+    MP2Contrast() {}
+    ~MP2Contrast() {}
+    bool operator!=(const MP2Contrast &) const { return false; }
+    bool operator==(const MP2Contrast &other) const { return !(*this != other); }
 
-    inline T operator()(const complex<T> &ti1,
-                              const complex<T> &ti2) const
+    inline complex<T> operator()(const complex<T> &ti1, const complex<T> &ti2) const
     {
         const T a1 = abs(ti1);
         const T a2 = abs(ti2);
-        return real((conj(ti1)*ti2)/(a1*a1 + a2*a2));
+        return (conj(ti1)*ti2)/(a1*a1 + a2*a2);
     }
 };
-
 
 int main(int argc, char **argv) {
     int indexptr = 0, c;
     string outPrefix = "";
-    bool verbose = false;
+    bool verbose = false, complex_output = false;
+    float thresh_quantile = 0.0;
     QI::ImageF::Pointer mask = ITK_NULLPTR;
     while ((c = getopt_long(argc, argv, short_options, long_options, &indexptr)) != -1) {
         switch (c) {
@@ -82,6 +89,8 @@ int main(int argc, char **argv) {
                 outPrefix = optarg;
                 cout << "Output prefix will be: " << outPrefix << endl;
                 break;
+            case 't': thresh_quantile = atof(optarg); break;
+            case 'x': complex_output = true; break;
             case 'T': itk::MultiThreader::SetGlobalDefaultNumberOfThreads(atoi(optarg)); break;
             case 'h':
                 cout << usage << endl;
@@ -128,7 +137,7 @@ int main(int argc, char **argv) {
         histFilter->SetHistogramSize(size);
         histFilter->SetAutoMinimumMaximum(true);
         histFilter->Update();
-        float threshold = histFilter->GetOutput()->Quantile(0, 0.9);
+        float threshold = histFilter->GetOutput()->Quantile(0, thresh_quantile);
         threshFilter->SetInput(magFilter->GetOutput());
         threshFilter->SetLowerThreshold(threshold);
         threshFilter->SetInsideValue(1);
@@ -140,15 +149,22 @@ int main(int argc, char **argv) {
 
     for (int i1 = 0; i1 < 3; i1++) {
         for (int i2 = (i1 + 1); i2 < 3; i2++) {
-            auto mp2rage_filter = itk::BinaryFunctorImageFilter<QI::ImageXF, QI::ImageXF, QI::ImageF, MP2RAGE<float>>::New();
+            auto mp2rage_filter = itk::BinaryFunctorImageFilter<QI::ImageXF, QI::ImageXF, QI::ImageXF, MP2Contrast<float>>::New();
             mp2rage_filter->SetInput1(vols[i1]->GetOutput());
             mp2rage_filter->SetInput2(vols[i2]->GetOutput());
 
-            auto mask_filter = itk::MaskImageFilter<QI::ImageF, QI::ImageF, QI::ImageF>::New();
+            auto mask_filter = itk::MaskImageFilter<QI::ImageXF, QI::ImageF, QI::ImageXF>::New();
             mask_filter->SetInput1(mp2rage_filter->GetOutput());
             mask_filter->SetMaskImage(mask);
             mask_filter->Update();
-            QI::writeResult<QI::ImageF>(mask_filter->GetOutput(), outPrefix + "_C" + to_string(i1) + to_string(i2) + QI::OutExt());
+            string outName = outPrefix + "_C" + to_string(i1) + to_string(i2) + QI::OutExt();
+            if (complex_output) {
+                QI::writeResult<QI::ImageXF>(mask_filter->GetOutput(), outName);
+            } else {
+                auto realFilter = itk::ComplexToRealImageFilter<QI::ImageXF, QI::ImageF>::New();
+                realFilter->SetInput(mask_filter->GetOutput());
+                QI::writeResult<QI::ImageF>(realFilter->GetOutput(), outName);
+            }
         }
     }
     if (verbose) cout << "Finished." << endl;
