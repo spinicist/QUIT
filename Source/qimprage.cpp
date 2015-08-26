d91a1de2b59090154d3ee834ba4182a113431ae2
Tobias Wood <tobias@spinicist.org.uk>
2015-08-26 22:36:14 +0100

    Hacked together T1&B1 fitting for MP3RAGE. Seems to fit T1 okay, B1 poorly and inversion efficiency (eta) not at all.

--- Source/qimprage.cpp
+++ Source/qimprage.cpp
@@ -15,15 +15,17 @@
 #include <complex>
 #include <getopt.h>
 
-#include "Types.h"
-#include "Util.h"
-
 #include "itkBinaryFunctorImageFilter.h"
 #include "itkImageToHistogramFilter.h"
 #include "itkComplexToModulusImageFilter.h"
 #include "itkComplexToRealImageFilter.h"
 #include "itkBinaryThresholdImageFilter.h"
 #include "itkMaskImageFilter.h"
+
+#include "Filters/ApplyAlgorithmFilter.h"
+#include "Types.h"
+#include "Util.h"
+#include "Sequence.h"
 
 using namespace std;
 
@@ -65,6 +67,64 @@ public:
         const T a1 = abs(ti1);
         const T a2 = abs(ti2);
         return (conj(ti1)*ti2)/(a1*a1 + a2*a2);
+    }
+};
+
+class MP3LookupAlgo : public Algorithm<double> {
+protected:
+    std::vector<Array3d> m_pars, m_cons;
+
+public:
+    size_t numInputs() const override { return 1; }
+    size_t numConsts() const override { return 0; }
+    size_t numOutputs() const override { return 3; }
+    size_t dataSize() const override { return 3; }
+
+    virtual TArray defaultConsts() {
+        // B1
+        TArray def = TArray::Ones(0);
+        return def;
+    }
+
+    MP3LookupAlgo() {
+        cout << "Build lookup table" << endl;
+        MP3RAGE sequence(true);
+        MP2Contrast<double> con;
+        m_pars.clear();
+        m_cons.clear();
+        for (float T1 = 0.5; T1 < 4.3; T1 += 0.005) {
+            for (float B1 = 0.75; B1 < 1.25; B1 += 0.01) {
+                for (float eta = 1.0; eta < 1.25; eta += 1.0) {
+                    Array3d tp; tp << T1, B1, eta;
+                    m_pars.push_back(tp);
+                    Array3cd sig = sequence.signal(1., T1, B1, eta);
+                    Array3d tc;
+                    tc[0] = con(sig[0], sig[1]).real();
+                    tc[1] = con(sig[0], sig[2]).real();
+                    tc[2] = con(sig[1], sig[2]).real();
+                    m_cons.push_back(tc);
+                    //cout << m_pars.back().transpose() << " : " << m_cons.back().transpose() << endl;
+                }
+            }
+        }
+        cout << "Lookup table has " << m_pars.size() << " entries" << endl;
+    }
+
+    virtual void apply(const TInput &data_inputs, const TArray &const_inputs,
+                       TArray &outputs, TArray &resids, TIterations &its) const override {
+        double best_distance = numeric_limits<double>::max();
+        int best_index = 0;
+
+        for (int i = 0; i < m_pars.size(); i++) {
+            double distance = (m_cons[i] - data_inputs).matrix().norm();
+            if (distance < best_distance) {
+                best_distance = distance;
+                best_index = i;
+            }
+        }
+        outputs = m_pars[best_index];
+        //cout << "Best index " << best_index << " distance " << best_distance << " pars " << outputs.transpose() << " data " << data_inputs.transpose() << " cons" << m_cons[best_index].transpose() << endl;
+        its = 1;
     }
 };
 
@@ -147,6 +207,9 @@ int main(int argc, char **argv) {
         mask->DisconnectPipeline();
     }
 
+    cout << "Generating contrast images" << endl;
+    vector<QI::ImageF::Pointer> conImages(3, ITK_NULLPTR);
+    int ind = 0;
     for (int i1 = 0; i1 < 3; i1++) {
         for (int i2 = (i1 + 1); i2 < 3; i2++) {
             auto mp2rage_filter = itk::BinaryFunctorImageFilter<QI::ImageXF, QI::ImageXF, QI::ImageXF, MP2Contrast<float>>::New();
@@ -157,16 +220,37 @@ int main(int argc, char **argv) {
             mask_filter->SetInput1(mp2rage_filter->GetOutput());
             mask_filter->SetMaskImage(mask);
             mask_filter->Update();
-            string outName = outPrefix + "_C" + to_string(i1) + to_string(i2) + QI::OutExt();
+            string outName = outPrefix + "MP3_C" + to_string(i1) + to_string(i2) + QI::OutExt();
+            auto realFilter = itk::ComplexToRealImageFilter<QI::ImageXF, QI::ImageF>::New();
+            realFilter->SetInput(mask_filter->GetOutput());
+            realFilter->Update();
+            conImages[ind] = realFilter->GetOutput();
+            conImages[ind]->DisconnectPipeline();
             if (complex_output) {
                 QI::writeResult<QI::ImageXF>(mask_filter->GetOutput(), outName);
             } else {
-                auto realFilter = itk::ComplexToRealImageFilter<QI::ImageXF, QI::ImageF>::New();
-                realFilter->SetInput(mask_filter->GetOutput());
                 QI::writeResult<QI::ImageF>(realFilter->GetOutput(), outName);
             }
+            ind++;
         }
     }
+
+    cout << "Attempting quantitative bit" << endl;
+    auto apply = itk::ApplyAlgorithmFilter<MP3LookupAlgo>::New();
+    auto lookup = make_shared<MP3LookupAlgo>();
+    apply->SetAlgorithm(lookup);
+
+    typedef itk::ComposeImageFilter<QI::ImageF, QI::VectorImageF> ComposeType;
+    auto composer = ComposeType::New();
+    composer->SetInput(0, conImages[0]);
+    composer->SetInput(1, conImages[1]);
+    composer->SetInput(2, conImages[2]);
+    apply->SetInput(0, composer->GetOutput());
+    apply->SetMask(mask);
+    apply->Update();
+    QI::writeResult(apply->GetOutput(0), outPrefix + "MP3_T1" + QI::OutExt());
+    QI::writeResult(apply->GetOutput(1), outPrefix + "MP3_B1" + QI::OutExt());
+    QI::writeResult(apply->GetOutput(2), outPrefix + "MP3_eta" + QI::OutExt());
     if (verbose) cout << "Finished." << endl;
     return EXIT_SUCCESS;
 }
