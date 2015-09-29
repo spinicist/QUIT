eb62a5472b980607f8025aeb14910393c95ffcc7
Tobias Wood <tobias@spinicist.org.uk>
2015-09-29 16:07:52 +0100

    Made the T1/B1/eta maps optional.

--- Source/qimprage.cpp
+++ Source/qimprage.cpp
@@ -35,6 +35,7 @@ Options:\n\
     --mask, -m file   : Mask input with specified file.\n\
     --out, -o path    : Add a prefix to the output filenames.\n\
     --complex, -x     : Output complex contast images.\n\
+    --quant, -q       : Generate T1, B1 and inversion efficiency maps\n\
     --threads, -T N   : Use a maximum of N threads.\n"
 };
 
@@ -43,10 +44,11 @@ static struct option long_options[] = {
     {"mask",    required_argument, 0, 'm'},
     {"out",     required_argument, 0, 'o'},
     {"complex", no_argument, 0, 'x'},
+    {"quant", no_argument, 0, 'q'},
     {"threads", required_argument, 0, 'T'},
     {0, 0, 0, 0}
 };
-static const char *short_options = "vm:o:xT:h";
+static const char *short_options = "vm:o:xqT:h";
 
 template<class T> class MP2Contrast {
 public:
@@ -63,15 +65,41 @@ public:
     }
 };
 
-class MP3LookupAlgo : public Algorithm<complex<double>> {
+class MPRAGEAlgo : public Algorithm<complex<double>> {
 protected:
     std::vector<Array3d> m_pars, m_cons;
+    bool m_do_lookup = false;
 
 public:
     size_t numInputs() const override { return 1; }
     size_t numConsts() const override { return 0; }
     size_t numOutputs() const override { return 6; }
     size_t dataSize() const override { return 3; }
+    void setDoLookup(const bool l) {
+        m_do_lookup = l;
+        if (m_do_lookup) {
+            MP3RAGE sequence(true);
+            MP2Contrast<double> con;
+            m_pars.clear();
+            m_cons.clear();
+            for (float T1 = 0.5; T1 < 2.5; T1 += 0.01) {
+                for (float B1 = 0.75; B1 < 1.25; B1 += 0.05) {
+                    for (float eta = 0.5; eta < 1.5; eta += 0.05) {
+                        Array3d tp; tp << T1, B1, eta;
+                        m_pars.push_back(tp);
+                        Array3cd sig = sequence.signal(1., T1, B1, eta);
+                        Array3d tc;
+                        tc[0] = con(sig[0], sig[1]).real();
+                        tc[1] = con(sig[0], sig[2]).real();
+                        tc[2] = con(sig[1], sig[2]).real();
+                        m_cons.push_back(tc);
+                        //cout << m_pars.back().transpose() << " : " << m_cons.back().transpose() << endl;
+                    }
+                }
+            }
+            cout << "Lookup table has " << m_pars.size() << " entries" << endl;
+        }
+    }
 
     virtual TArray defaultConsts() override {
         // B1
@@ -79,28 +107,7 @@ public:
         return def;
     }
 
-    MP3LookupAlgo() {
-        cout << "Build lookup table" << endl;
-        MP3RAGE sequence(true);
-        MP2Contrast<double> con;
-        m_pars.clear();
-        m_cons.clear();
-        for (float T1 = 0.5; T1 < 2.5; T1 += 0.005) {
-            for (float B1 = 0.75; B1 < 1.25; B1 += 0.01) {
-                for (float eta = 0.5; eta < 1.5; eta += 0.05) {
-                    Array3d tp; tp << T1, B1, eta;
-                    m_pars.push_back(tp);
-                    Array3cd sig = sequence.signal(1., T1, B1, eta);
-                    Array3d tc;
-                    tc[0] = con(sig[0], sig[1]).real();
-                    tc[1] = con(sig[0], sig[2]).real();
-                    tc[2] = con(sig[1], sig[2]).real();
-                    m_cons.push_back(tc);
-                    //cout << m_pars.back().transpose() << " : " << m_cons.back().transpose() << endl;
-                }
-            }
-        }
-        cout << "Lookup table has " << m_pars.size() << " entries" << endl;
+    MPRAGEAlgo() {
     }
 
     virtual void apply(const TInput &data_inputs, const TArray &const_inputs,
@@ -113,17 +120,22 @@ public:
         in_cons[0] = con(data_inputs[0], data_inputs[1]).real();
         in_cons[1] = con(data_inputs[0], data_inputs[2]).real();
         in_cons[2] = con(data_inputs[1], data_inputs[2]).real();
-
-        for (int i = 0; i < m_pars.size(); i++) {
-            double distance = (m_cons[i] - in_cons).matrix().norm();
-            if (distance < best_distance) {
-                best_distance = distance;
-                best_index = i;
-            }
-        }
         outputs.head(3) = in_cons;
-        outputs.tail(3) = m_pars[best_index];
-        resids = in_cons - m_cons[best_index];
+
+        if (m_do_lookup) {
+            for (int i = 0; i < m_pars.size(); i++) {
+                double distance = (m_cons[i] - in_cons).matrix().norm();
+                if (distance < best_distance) {
+                    best_distance = distance;
+                    best_index = i;
+                }
+            }
+            outputs.tail(3) = m_pars[best_index];
+            resids = in_cons - m_cons[best_index];
+        } else {
+            outputs.tail(3).setConstant(0);
+            resids.setConstant(0);
+        }
         //cout << "Best index " << best_index << " distance " << best_distance << " pars " << outputs.transpose() << " data " << data_inputs.transpose() << " cons" << m_cons[best_index].transpose() << endl;
         its = 1;
     }
@@ -132,7 +144,7 @@ public:
 int main(int argc, char **argv) {
     int indexptr = 0, c;
     string outPrefix = "";
-    bool verbose = false, complex_output = false;
+    bool verbose = false, complex_output = false, do_lookup = false;
     QI::ImageF::Pointer mask = ITK_NULLPTR;
     while ((c = getopt_long(argc, argv, short_options, long_options, &indexptr)) != -1) {
         switch (c) {
@@ -150,6 +162,7 @@ int main(int argc, char **argv) {
                 cout << "Output prefix will be: " << outPrefix << endl;
                 break;
             case 'x': complex_output = true; break;
+            case 'q': do_lookup = true; break;
             case 'T': itk::MultiThreader::SetGlobalDefaultNumberOfThreads(atoi(optarg)); break;
             case 'h':
                 cout << usage << endl;
@@ -170,10 +183,11 @@ int main(int argc, char **argv) {
     auto inFile = QI::ReadTimeseriesXF::New();
     inFile->SetFileName(fname);
     inFile->Update();
-    cout << "Attempting quantitative bit" << endl;
-    auto apply = itk::ApplyAlgorithmFilter<MP3LookupAlgo, complex<float>>::New();
-    auto lookup = make_shared<MP3LookupAlgo>();
-    apply->SetAlgorithm(lookup);
+    if (verbose) cout << "Processing" << endl;
+    auto apply = itk::ApplyAlgorithmFilter<MPRAGEAlgo, complex<float>>::New();
+    auto process = make_shared<MPRAGEAlgo>();
+    process->setDoLookup(do_lookup);
+    apply->SetAlgorithm(process);
 
     auto vectorFilter = QI::TimeseriesToVectorXF::New();
     vectorFilter->SetInput(inFile->GetOutput());
@@ -183,10 +197,12 @@ int main(int argc, char **argv) {
     QI::writeResult(apply->GetOutput(0), outPrefix + "MP3_C12" + QI::OutExt());
     QI::writeResult(apply->GetOutput(1), outPrefix + "MP3_C13" + QI::OutExt());
     QI::writeResult(apply->GetOutput(2), outPrefix + "MP3_C23" + QI::OutExt());
-    QI::writeResult(apply->GetOutput(3), outPrefix + "MP3_T1" + QI::OutExt());
-    QI::writeResult(apply->GetOutput(4), outPrefix + "MP3_B1" + QI::OutExt());
-    QI::writeResult(apply->GetOutput(5), outPrefix + "MP3_eta" + QI::OutExt());
-    QI::writeResiduals(apply->GetResidOutput(), outPrefix + "MP3_", true);
+    if (do_lookup) {
+        QI::writeResult(apply->GetOutput(3), outPrefix + "MP3_T1" + QI::OutExt());
+        QI::writeResult(apply->GetOutput(4), outPrefix + "MP3_B1" + QI::OutExt());
+        QI::writeResult(apply->GetOutput(5), outPrefix + "MP3_eta" + QI::OutExt());
+        QI::writeResiduals(apply->GetResidOutput(), outPrefix + "MP3_", true);
+    }
     if (verbose) cout << "Finished." << endl;
     return EXIT_SUCCESS;
 }
