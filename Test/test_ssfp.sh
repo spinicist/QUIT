f1c1b6655542d88905496c6c39c921eee0c1b517
Tobias Wood <tobias@spinicist.org.uk>
2015-11-02 09:17:51 +0000

    Tweaked the SSFP tests.

--- Test/test_ssfp.sh
+++ Test/test_ssfp.sh
@@ -4,7 +4,7 @@
 # Test script for ssfpbands etc.
 
 source ./test_common.sh
-SILENCE_TESTS="1"
+SILENCE_TESTS="0"
 
 # First, create input data
 DATADIR="ssfp"
@@ -14,26 +14,27 @@ if [ "$(ls -A ./)" ]; then
     rm *
 fi
 
-DIMS="16 16 25"
+DIMS="32 32 101"
 VOXDIMS="2 2 2"
 $QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -f "1.0" PD.nii
 $QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -f "1.0" T1.nii
 $QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -f "0.1" T2.nii
-$QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -g "2 -25.0 25.0" f0.nii
-$QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -g "2 0.75 1.25" B1.nii
+$QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -g "2 -100.0 100.0" f0.nii
+$QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -f "1.0" B1.nii
 
 SSFP_FILE="ssfp.nii"
+SSFP_MAG="ssfp_mag.nii"
 SSFP_OUT_FILE="ssfp_lreg.nii"
-SSFP_MAG_FILE="ssfp_lreg_mag.nii"
-SSFP_PAR="15 30 45
+SSFP_OUT_MAG="ssfp_lreg_mag.nii"
+SSFP_PAR="5 10 15 20 25 30 35 40 45 50 55 60
 0 90 180 270
 0.005"
 GS_FILE="gs.nii"
 GS_MAG_FILE="gs_mag.nii"
-GS_PAR="15 30 45
+GS_PAR="5 10 15 20 25 30 35 40 45 50 55 60
 0.005"
 
-run_test "CREATE_COMPLEX_SIGNALS" $QUITDIR/qisignal --1 -n -x <<END_IN
+run_test "CREATE_COMPLEX_SIGNALS" $QUITDIR/qisignal --1 -n -v -N 0.001 -x <<END_IN
 PD.nii
 T1.nii
 T2.nii
@@ -49,6 +50,7 @@ END
 END_IN
 
 run_test "SSFPGS" $QUITDIR/qissfpbands $SSFP_FILE
-$QUITDIR/qicomplex -x $SSFP_OUT_FILE -M $SSFP_MAG_FILE
+$QUITDIR/qicomplex -x $SSFP_FILE -M $SSFP_MAG
+$QUITDIR/qicomplex -x $SSFP_OUT_FILE -M $SSFP_OUT_MAG
 $QUITDIR/qicomplex -x $GS_FILE -M $GS_MAG_FILE
-compare_test "DESPOT2GS" $GS_MAG_FILE $SSFP_MAG_FILE 0.01
+compare_test "DESPOT2GS" $GS_MAG_FILE $SSFP_OUT_MAG 0.01
