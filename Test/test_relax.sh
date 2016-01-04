992e1515844ed287ec9812b05a72b4ac79eed1ee
Tobias Wood <tobias@spinicist.org.uk>
2016-01-04 12:19:54 +0000

    Made tests a bit more challenging.

--- Test/test_relax.sh
+++ Test/test_relax.sh
@@ -13,15 +13,15 @@ if [ "$(ls -A ./)" ]; then
     rm *
 fi
 
-DIMS="11 11 11"
+DIMS="11 11 101"
 $QUITDIR/qinewimage -d "$DIMS" -f 1 PD.nii
-$QUITDIR/qinewimage -d "$DIMS" -g "0 0.5 5" T1.nii
-$QUITDIR/qinewimage -d "$DIMS" -g "1 0.05 0.5" T2.nii
+$QUITDIR/qinewimage -d "$DIMS" -g "0 0.5 1.5" T1.nii
+$QUITDIR/qinewimage -d "$DIMS" -g "1 0.01 0.1" T2.nii
 $QUITDIR/qinewimage -d "$DIMS" -g "2 -25.0 25.0" f0.nii
 $QUITDIR/qinewimage -d "$DIMS" -f 1 B1.nii
 # Setup parameters
-SPIN_FILE="multispinecho.nii"
-SPIN_PAR="5.0
+SPIN_FILE="me.nii"
+SPIN_PAR="2.5
 0.005
 0.005
 16"
@@ -37,17 +37,17 @@ SPINECHO
 $SPIN_PAR
 END"
 echo "$MCSIG_INPUT" > qisignal.in
-run_test "CREATE_SIGNALS" $QUITDIR/qisignal --1 < qisignal.in
+run_test "CREATE_SIGNALS" $QUITDIR/qisignal --1 --noise 0.04 < qisignal.in
 
-echo "$SPIN_PAR" > multiecho_spin.in
+echo "$SPIN_PAR" > multiecho.in
 
-run_test "SPINECHO_LOGLIN" $QUITDIR/qimultiecho $SPIN_FILE -n -al -oL -v < multiecho_spin.in
-run_test "SPINECHO_ARLO"   $QUITDIR/qimultiecho $SPIN_FILE -n -aa -oA -v -T1 < multiecho_spin.in
-run_test "SPINECHO_NONLIN" $QUITDIR/qimultiecho $SPIN_FILE -n -an -oN -v < multiecho_spin.in
-compare_test "LOGLIN" T2.nii LME_T2.nii 0.01
-compare_test "ARLO" T2.nii AME_T2.nii 0.01
-compare_test "NONLIN" T2.nii NME_T2.nii 0.01
+run_test "SPINECHO_LOGLIN" $QUITDIR/qimultiecho $SPIN_FILE -n -v -al -oLL_     < multiecho.in
+run_test "SPINECHO_LEVMAR" $QUITDIR/qimultiecho $SPIN_FILE -n -v -an -oLEVMAR_ < multiecho.in
+run_test "SPINECHO_ARLO"   $QUITDIR/qimultiecho $SPIN_FILE -n -v -aa -oARLO_   < multiecho.in
 
+compare_test "LOGLIN" T2.nii LL_ME_T2.nii     0.005
+compare_test "LEVMAR" T2.nii LEVMAR_ME_T2.nii 0.005
+compare_test "ARLO"   T2.nii ARLO_ME_T2.nii   0.005
 
 cd ..
 SILENCE_TESTS="0"
