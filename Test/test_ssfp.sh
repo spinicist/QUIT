0445b0da0e17d4ead49e21f96e2caa22aa63957c
Tobias Wood <tobias@spinicist.org.uk>
2016-01-06 16:18:50 +0000

    Updated tests. Currently the comparison tests fail.

--- Test/test_ssfp.sh
+++ Test/test_ssfp.sh
@@ -24,17 +24,18 @@ $QUITDIR/qinewimage -d "$DIMS" -v "$VOXDIMS" -f "1.0" B1.nii
 
 SSFP_FILE="ssfp.nii"
 SSFP_MAG="ssfp_mag.nii"
-SSFP_OUT_FILE="ssfp_nobands.nii"
-SSFP_OUT_MAG="ssfp_nobands_mag.nii"
 SSFP_PAR="5 10 15 20 25 30 35 40 45 50 55 60
 0 90 180 270
 0.005"
 GS_FILE="gs.nii"
-GS_MAG_FILE="gs_mag.nii"
+CS_FILE="cs.nii"
+SUM_FILE="sum.nii"
+SOS_FILE="sos.nii"
+MAX_FILE="max.nii"
 GS_PAR="5 10 15 20 25 30 35 40 45 50 55 60
 0.005"
 
-run_test "CREATE_COMPLEX_SIGNALS" $QUITDIR/qisignal --1 -n -v -N 0.001 -x <<END_IN
+run_test "CREATE_COMPLEX_SIGNALS" $QUITDIR/qisignal --1 -n -N 0.001 -x <<END_IN
 PD.nii
 T1.nii
 T2.nii
@@ -49,8 +50,15 @@ $GS_PAR
 END
 END_IN
 
-run_test "SSFPGS" $QUITDIR/qissfpbands $SSFP_FILE -R L -2
+run_test "GS" $QUITDIR/qissfpbands $SSFP_FILE -R L -2 --magnitude -o $GS_FILE
+run_test "CS" $QUITDIR/qissfpbands $SSFP_FILE --cs --magnitude -o $CS_FILE
+run_test "SUM" $QUITDIR/qissfpbands $SSFP_FILE --magsum --magnitude -o $SUM_FILE
+run_test "SOS" $QUITDIR/qissfpbands $SSFP_FILE --sos --magnitude -o $SOS_FILE
+run_test "MAX" $QUITDIR/qissfpbands $SSFP_FILE --max --magnitude -o $MAX_FILE
+
 $QUITDIR/qicomplex -x $SSFP_FILE -M $SSFP_MAG
-$QUITDIR/qicomplex -x $SSFP_OUT_FILE -M $SSFP_OUT_MAG
-$QUITDIR/qicomplex -x $GS_FILE -M $GS_MAG_FILE
-compare_test "DESPOT2GS" $GS_MAG_FILE $SSFP_OUT_MAG 0.01
+compare_test "GS" $GS_FILE $SSFP_MAG 0.01
+compare_test "CS" $CS_FILE $SSFP_MAG 0.01
+compare_test "SUM" $SUM_FILE $SSFP_MAG 0.01
+compare_test "SOS" $SOS_FILE $SSFP_MAG 0.01
+compare_test "MAX" $MAX_FILE $SSFP_MAG 0.01
