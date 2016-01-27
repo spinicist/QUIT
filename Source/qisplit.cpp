0327259da0f012a81210bffe043dd06c0bdf0692
Tobias Wood <tobias@spinicist.org.uk>
2016-01-27 11:05:49 +0000

    First pass at adding CoG and transformation calculation.

--- Source/qisplit.cpp
+++ Source/qisplit.cpp
@@ -23,31 +23,36 @@
 #include "itkRelabelComponentImageFilter.h"
 #include "itkLabelStatisticsImageFilter.h"
 #include "itkExtractImageFilter.h"
+#include "itkImageMomentsCalculator.h"
+#include "itkAffineTransform.h"
+#include "itkTransformFileWriter.h"
 
 using namespace std;
-
-const string usage {
-"Usage is: qisplit input_file.nii [options]\n\
-\n\
-Options:\n\
-	--help, -h    : Print this message\n\
-	--verbose, -v : Print more information\n\
-	--keep, -k   : Keep N largest objects (default 4)\n"
-};
-
-const struct option long_options[] =
-{
-	{"help", no_argument, 0, 'h'},
-	{"verbose", no_argument, 0, 'v'},
-	{"keep", required_argument, 0, 'k'},
-	{0, 0, 0, 0}
-};
-const char* short_options = "hvk:";
 
 int main(int argc, char **argv) {
 	bool verbose = false;
 	int indexptr = 0, c;
-	int keep = 4;
+	int keep = 4, inwards = false;
+	const string usage {
+	"Usage is: qisplit input_file.nii [options]\n\
+	\n\
+	Options:\n\
+		--help, -h    : Print this message\n\
+		--verbose, -v : Print more information\n\
+		--keep, -k    : Keep N largest objects (default 4)\n\
+		--inwards     : Subjects were scanned facing 'inwards'\n"
+	};
+
+	const struct option long_options[] =
+	{
+		{"help", no_argument, 0, 'h'},
+		{"verbose", no_argument, 0, 'v'},
+		{"keep", required_argument, 0, 'k'},
+		{"inwards", no_argument, &inwards, true},
+		{0, 0, 0, 0}
+	};
+	const char* short_options = "hvk:";
+
 	while ((c = getopt_long(argc, argv, short_options, long_options, &indexptr)) != -1) {
 		switch (c) {
 		case 'v': verbose = true; break;
@@ -56,6 +61,8 @@ int main(int argc, char **argv) {
 			return EXIT_SUCCESS;
 		case 'k':
 			keep = atoi(optarg);
+			break;
+		case 0: // longopts flag
 			break;
 		case '?': // getopt will print an error message
 			return EXIT_FAILURE;
@@ -116,17 +123,45 @@ int main(int argc, char **argv) {
 	labelStats->SetLabelInput(relabel->GetOutput());
 	labelStats->Update();
 	
+	itk::Vector<double, 3> zAxis; zAxis.Fill(0); zAxis[2] = 1.0;
 	for (auto i = 1; i <= 4; i++) {
 		TLabelImage::RegionType region = labelStats->GetRegion(i);
 		auto extract = itk::ExtractImageFilter<QI::ImageF, QI::ImageF>::New();
 		extract->SetInput(input->GetOutput());
 		extract->SetExtractionRegion(region);
 		extract->SetDirectionCollapseToSubmatrix();
-		if (verbose) cout << "Writing object " << i << " region is " << region << endl;
+		extract->Update();
+
+		typedef itk::ImageMomentsCalculator<QI::ImageF> TMoments;
+		auto moments = TMoments::New();
+		moments->SetImage(extract->GetOutput());
+		moments->Compute();
+		TMoments::VectorType CoG = moments->GetCenterOfGravity();
+		if (verbose) cout << "Writing object " << i << " CoG is " << CoG << endl;
+		float rotateAngle = atan2(CoG[1], CoG[0]);
+		if (inwards)
+			rotateAngle = (M_PI / 2.) - rotateAngle;
+		else
+			rotateAngle = (M_PI * 3./2.) - rotateAngle;
+		if (verbose) cout << "Rotation angle is " << (rotateAngle*180./M_PI) << " degrees" << endl;
+
+		typedef itk::AffineTransform<double, 3> TAffine;
+		auto tfm = TAffine::New();
+		tfm->SetIdentity();
+		tfm->SetOffset(-CoG);
+		tfm->Rotate3D(zAxis, rotateAngle, 1);
+
+		if (verbose) cout << "Transform is: " << tfm << endl;
+
 		auto output = itk::ImageFileWriter<QI::ImageF>::New();
 		output->SetInput(extract->GetOutput());
 		output->SetFileName("image_" + to_string(i) + ".nii");
 		output->Update();
+
+  		auto tfmWriter = itk::TransformFileWriterTemplate<double>::New();
+  		tfmWriter->SetInput(tfm);
+  		tfmWriter->SetFileName("image_" + to_string(i) + ".tfm");
+  		tfmWriter->Update();
 	}
 	return EXIT_SUCCESS;
 }
