32a21100e0e211f038dc1a2c369bb11319cf9d59
Tobias Wood <tobias@spinicist.org.uk>
2016-01-29 12:35:30 +0000

    Big refactoring, breaking steps out into functions. Added back intensity threshold for masking step. Added an external mask option.

--- Source/qisplitsubjects.cpp
+++ Source/qisplitsubjects.cpp
@@ -36,8 +36,67 @@
 
 using namespace std;
 
-typedef itk::ImageMomentsCalculator<QI::ImageF> TMoments;
+typedef unsigned int TLabel;
+typedef itk::Image<TLabel, 3> TLabelImage;
 
+typename TLabelImage::Pointer ThresholdMask(const QI::ImageF::Pointer &img, const float thresh) {
+    typedef itk::BinaryThresholdImageFilter<QI::ImageF, TLabelImage> TThreshFilter;
+    auto threshold = TThreshFilter::New();
+    threshold->SetInput(img);
+    threshold->SetLowerThreshold(thresh);
+    threshold->SetUpperThreshold(numeric_limits<float>::infinity());
+    threshold->SetInsideValue(1);
+    threshold->SetOutsideValue(0);
+    threshold->Update();
+    typename TLabelImage::Pointer mask = threshold->GetOutput();
+    mask->DisconnectPipeline();
+    return mask;
+}
+
+typename TLabelImage::Pointer OtsuMask(const QI::ImageF::Pointer &img) {
+    auto otsuFilter = itk::OtsuThresholdImageFilter<QI::ImageF, TLabelImage>::New();
+    otsuFilter->SetInput(img);
+    otsuFilter->SetOutsideValue(1);
+    otsuFilter->SetInsideValue(0);
+    otsuFilter->Update();
+    typename TLabelImage::Pointer mask = otsuFilter->GetOutput();
+    mask->DisconnectPipeline();
+    return mask;
+}
+
+typename TLabelImage::Pointer FindLabels(const TLabelImage::Pointer &mask, const int size_threshold, int &keep) {
+    auto CC = itk::ConnectedComponentImageFilter<TLabelImage, TLabelImage>::New();
+    auto relabel = itk::RelabelComponentImageFilter<TLabelImage, TLabelImage>::New();
+    CC->SetInput(mask);
+    relabel->SetInput(CC->GetOutput());
+    relabel->Update();
+    // Relabel sorts on size by default, so now work out how many make the size threshold
+    auto label_sizes = relabel->GetSizeOfObjectsInPixels();
+    if (keep > label_sizes.size())
+        keep = label_sizes.size();
+    for (int i = 0; i < keep; i++) {
+        if (label_sizes[i] < size_threshold) {
+            keep = i;
+            break;
+        }
+    }
+    if (keep == 0) {
+        throw(runtime_error("No labels found in mask"));
+    }
+
+    typedef itk::LabelShapeKeepNObjectsImageFilter<TLabelImage> TKeepN;
+    auto keepN = TKeepN::New();
+    keepN->SetInput(relabel->GetOutput());
+    keepN->SetBackgroundValue(0);
+    keepN->SetNumberOfObjects(keep);
+    keepN->SetAttribute(TKeepN::LabelObjectType::NUMBER_OF_PIXELS);
+    keepN->Update();
+    typename TLabelImage::Pointer labels = keepN->GetOutput();
+    labels->DisconnectPipeline();
+    return labels;
+}
+
+typedef itk::ImageMomentsCalculator<QI::ImageF> TMoments;
 TMoments::VectorType GetCoG(const QI::ImageF::Pointer &img) {
     auto moments = TMoments::New();
     moments->SetImage(img);
@@ -49,28 +108,35 @@ const string usage {
 "Usage is: qisplit input_file.nii [options]\n\
 \n\
 Options:\n\
-	--help, -h      : Print this message\n\
-	--verbose, -v   : Print more information\n\
-	--keep, -k N    : Keep N largest subjects\n\
-    --size, -s N    : Only keep subjects with >N voxels (default 1000)\n\
-	--ref, -r       : Specify a reference image for CoG\n\
-	--center=IN/OUT : Center and rotate subjects that were scanned in standard\n\
-	                  ring arrangement facing IN or OUT.\n\
-	--rotX N        : Rotate by N degrees around the X axis.\n\
-	--rotY N        : Rotate by N degrees around the Y axis.\n\
-	--rotZ N        : Rotate by N degrees around the Z axis.\n\
-	--oimgs         : Output images\n"
+    --help, -h     : Print this message\n\
+    --verbose, -v  : Print more information\n\
+    --keep, -k N   : Keep N largest subjects\n\
+    --size, -s N   : Only keep subjects with >N voxels (default 1000)\n\
+    --oimgs        : Output images (default only transforms)\n\
+    --ref, -r      : Specify a reference image for output space\n\
+Masking options (default is generate a mask with Otsu's method):\n\
+    --mask, -m F   : Read the mask from file F\n\
+    --thresh, -t N : Generate a mask by thresholding input at intensity N\n\
+Alignment/arrangement correction (default is no correction):\n\
+    --ring=IN/OUT  : Center and rotate subjects that were scanned in standard\n\
+                     ring arrangement facing IN or OUT.\n\
+    --rotX N       : Rotate by N degrees around the X axis.\n\
+    --rotY N       : Rotate by N degrees around the Y axis.\n\
+    --rotZ N       : Rotate by N degrees around the Z axis.\n\
+"
 };
-enum { CENTER_IN = 0, CENTER_OUT, CENTER_NONE};
+enum class ALIGN { NONE = 0, RING_IN, RING_OUT };
 
 int main(int argc, char **argv) {
 	bool verbose = false;
 	int indexptr = 0, c;
-	int keep = numeric_limits<int>::max(), size_threshold = 1000, output_images = false,
-	    center = CENTER_NONE;
+    int keep = numeric_limits<int>::max(), size_threshold = 1000, output_images = false;
+    ALIGN alignment = ALIGN::NONE;
+    float intensity_threshold = 0;
     double angleX = 0., angleY = 0., angleZ = 0.;
 
 	QI::ImageF::Pointer reference = ITK_NULLPTR;
+    TLabelImage::Pointer mask = ITK_NULLPTR;
 	
 	const struct option long_options[] =
 	{
@@ -78,8 +144,10 @@ int main(int argc, char **argv) {
 		{"verbose", no_argument, 0, 'v'},
 		{"keep", required_argument, 0, 'k'},
         {"size", required_argument, 0, 's'},
+        {"thresh", required_argument, 0, 't'},
+        {"mask", required_argument, 0, 'm'},
 		{"ref", required_argument, 0, 'r'},
-		{"center", required_argument, 0, 'c'},
+        {"ring", required_argument, 0, 'R'},
 		{"rotX", required_argument, 0, 'X'},
 		{"rotY", required_argument, 0, 'Y'},
 		{"rotZ", required_argument, 0, 'Z'},
@@ -92,20 +160,22 @@ int main(int argc, char **argv) {
 		switch (c) {
 		case 'v': verbose = true; break;
 		case 'h':
-			cout << usage << endl;
-			return EXIT_SUCCESS;
-		case 'r': reference = QI::ReadImage(optarg); break;
-		case 'c':
-			if (string(optarg) == "IN") {
-				center = CENTER_IN;
-			} else if (string(optarg) == "OUT") {
-				center = CENTER_OUT;
-			} else {
-				cerr << "Unrecognised center specifier: " << string(optarg) << endl;
-				return EXIT_FAILURE;
-			} break;
+            cout << usage << endl;
+            return EXIT_SUCCESS;
+        case 'r': reference = QI::ReadImage(optarg); break;
+        case 'm': mask = QI::ReadImage<TLabelImage>(optarg); break;
+        case 'R':
+            if (string(optarg) == "IN") {
+                alignment = ALIGN::RING_IN;
+            } else if (string(optarg) == "OUT") {
+                alignment = ALIGN::RING_OUT;
+            } else {
+                cerr << "Unrecognised ring alignment specifier: " << string(optarg) << endl;
+                return EXIT_FAILURE;
+            } break;
 		case 'k': keep = atoi(optarg); break;
         case 's': size_threshold = atoi(optarg); break;
+        case 't': intensity_threshold = stof(optarg); break;
         case 'X': angleX = stod(optarg)*M_PI/180.; break;
         case 'Y': angleY = stod(optarg)*M_PI/180.; break;
         case 'Z': angleZ = stod(optarg)*M_PI/180.; break;
@@ -124,65 +194,26 @@ int main(int argc, char **argv) {
 		throw(runtime_error("Wrong number of input arguments."));
 	}
 
-	auto input = QI::ImageReaderF::New();
-	string fname(argv[optind++]);
-	input->SetFileName(fname);
-	string prefix = QI::StripExt(fname);
+    string fname(argv[optind++]);
+    QI::ImageF::Pointer input = QI::ReadImage(fname);
+    string prefix = QI::StripExt(fname);
 
-	typedef unsigned int TLabel;
-	typedef itk::Image<TLabel, 3> TLabelImage;
-	/*typedef itk::BinaryThresholdImageFilter<QI::ImageF, TLabelImage> TThreshFilter;
-	auto threshold = TThreshFilter::New();
-	threshold->SetInput(input->GetOutput());
-	threshold->SetLowerThreshold(atof(argv[optind++]));
-	threshold->SetUpperThreshold(numeric_limits<float>::infinity());
-	threshold->SetInsideValue(1);
-	threshold->SetOutsideValue(0);*/
-    // Use an Otsu Threshold filter to generate the mask
-    if (verbose) cout << "Generating Otsu mask" << endl;
-    auto otsuFilter = itk::OtsuThresholdImageFilter<QI::ImageF, TLabelImage>::New();
-    otsuFilter->SetInput(input->GetOutput());
-    otsuFilter->SetOutsideValue(1);
-    otsuFilter->SetInsideValue(0);
-    otsuFilter->Update();
-
-	auto CC = itk::ConnectedComponentImageFilter<TLabelImage, TLabelImage>::New();
-	CC->SetInput(otsuFilter->GetOutput());
-	
-    typedef itk::RelabelComponentImageFilter<TLabelImage, TLabelImage> TRelabel;
-    auto relabel = TRelabel::New();
-    relabel->SetInput(CC->GetOutput());
-    relabel->Update();
-    // Relabel sorts on size by default, so now work out how many make the size threshold
-    auto label_sizes = relabel->GetSizeOfObjectsInPixels();
-    if (keep > label_sizes.size())
-        keep = label_sizes.size();
-    for (int i = 0; i < keep; i++) {
-        if (label_sizes[i] < size_threshold) {
-            keep = i;
-            break;
-        }
+    if (mask == ITK_NULLPTR) {
+        if (intensity_threshold == 0)
+            mask = OtsuMask(input);
+        else
+            mask = ThresholdMask(input, intensity_threshold);
     }
-    if (keep == 0) {
-        cerr << "Found 0 objects to keep. Exiting." << endl;
-        return EXIT_FAILURE;
-    }
-    if (verbose) cout << "Found " << keep << " objects to keep." << endl;
 
-	typedef itk::LabelShapeKeepNObjectsImageFilter<TLabelImage> TKeepN;
-	auto keepN = TKeepN::New();
-	keepN->SetInput(relabel->GetOutput());
-	keepN->SetBackgroundValue(0);
-	keepN->SetNumberOfObjects(keep);
-	keepN->SetAttribute(TKeepN::LabelObjectType::NUMBER_OF_PIXELS);
-
-    if (verbose) cout << "Writing label image." << endl;
-    QI::WriteImage<TLabelImage>(keepN->GetOutput(), prefix + "_labels.nii");
+    TLabelImage::Pointer labels = FindLabels(mask, size_threshold, keep);
+    if (verbose) cout << "Found " << keep << " subjects, saving labels." << endl;
+    cout << labels << endl;
+    QI::WriteImage<TLabelImage>(labels, prefix + "_labels.nii");
 
 	typedef itk::LabelStatisticsImageFilter<QI::ImageF, TLabelImage> TLabelStats;
 	auto labelStats = TLabelStats::New();
-	labelStats->SetInput(input->GetOutput());
-	labelStats->SetLabelInput(keepN->GetOutput());
+	labelStats->SetInput(input);
+	labelStats->SetLabelInput(labels);
 	labelStats->Update();
 
     TMoments::VectorType refCoG;
@@ -195,47 +226,47 @@ int main(int argc, char **argv) {
     typedef itk::ResampleImageFilter<TLabelImage, TLabelImage, double> TLabelResampler;
     typedef itk::NearestNeighborInterpolateImageFunction<TLabelImage, double> TLabelInterp;
     auto ilabels = TLabelInterp::New();
-    ilabels->SetInputImage(keepN->GetOutput());
+    ilabels->SetInputImage(labels);
     auto rlabels = TLabelResampler::New();
-    rlabels->SetInput(keepN->GetOutput());
+    rlabels->SetInput(labels);
     rlabels->SetInterpolator(ilabels);
     rlabels->SetDefaultPixelValue(0.);
     if (reference)
     	rlabels->SetOutputParametersFromImage(reference);
    	else
-    	rlabels->SetOutputParametersFromImage(relabel->GetOutput());
+    	rlabels->SetOutputParametersFromImage(labels);
 
     typedef itk::ResampleImageFilter<QI::ImageF, QI::ImageF, double> TResampleF;
     typedef itk::LinearInterpolateImageFunction<QI::ImageF, double> TLinearInterpF;
     auto interp = TLinearInterpF::New();
-    interp->SetInputImage(input->GetOutput());
+    interp->SetInputImage(input);
     auto rimage = TResampleF::New();
-    rimage->SetInput(input->GetOutput());
+    rimage->SetInput(input);
     rimage->SetInterpolator(interp);
     rimage->SetDefaultPixelValue(0.);
     if (reference)
     	rimage->SetOutputParametersFromImage(reference);
     else
-    	rimage->SetOutputParametersFromImage(input->GetOutput());
+    	rimage->SetOutputParametersFromImage(input);
 
 	for (auto i = 1; i <= keep; i++) {
 		TLabelImage::RegionType region = labelStats->GetRegion(i);
 		typedef itk::ExtractImageFilter<QI::ImageF, QI::ImageF> TExtractF;
 		auto extract = TExtractF::New();
-		extract->SetInput(input->GetOutput());
+		extract->SetInput(input);
 		extract->SetExtractionRegion(region);
 		extract->SetDirectionCollapseToSubmatrix();
 		extract->Update();
 
 		TMoments::VectorType offset = -refCoG;
 		double rotateAngle = 0.;
-		if (center != CENTER_NONE) {
+		if (alignment != ALIGN::NONE) {
             TMoments::VectorType CoG = GetCoG(extract->GetOutput());
 			if (verbose) cout << "Subject " << i << " CoG is " << CoG << endl;
 			rotateAngle = atan2(CoG[1], CoG[0]);
-			if (center == CENTER_IN)
+			if (alignment == ALIGN::RING_IN)
 				rotateAngle = (M_PI / 2.) - rotateAngle;
-			else if (center == CENTER_OUT)
+			else if (alignment == ALIGN::RING_OUT)
 				rotateAngle = (M_PI * 3./2.) - rotateAngle;
 			if (verbose) cout << "Rotation angle is " << (rotateAngle*180./M_PI) << " degrees" << endl;
 			offset += CoG;
