9c21fcedd87fa9999d492b9a3bd3a97194787f38
Tobias Wood <tobias@spinicist.org.uk>
2016-02-15 14:07:20 +0000

    Polising versioning system.

--- CMake/GetVersion.cmake
+++ CMake/GetVersion.cmake
@@ -1,22 +1,18 @@
 # Set up the version file
-FIND_PACKAGE(Git)
-SET( BUILD_VERSION 1.1 )
-IF( GIT_FOUND )
-    EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} describe --tags RESULT_VARIABLE GIT_RES OUTPUT_VARIABLE GIT_DESCRIPTION )
-    MESSAGE( STATUS ${GIT_RES} ${GIT_DESCRIPTION} )
-    IF( NOT ${GIT_RES} EQUAL 0 )
-        MESSAGE( WARNING "Git failed (not a repo, or no tags). Defaulting to static build number." )
-    ENDIF()
-    STRING( REPLACE "\n" "" BUILD_VERSION ${GIT_DESCRIPTION} )
-    MESSAGE( STATUS ${BUILD_VERSION} ${GIT_DESCRIPTION} )
-ENDIF( GIT_FOUND )
-MESSAGE( STATUS "Version: ${BUILD_VERSION}" )
+set( VERSION_FILE_NAME "IncludeVersion")
 
-SET( VERSION_STRING "//version_string.hpp - written by cmake. changes will be lost!\n"
-             "const std::string Version = \"${BUILD_VERSION}\"\;\n")
+find_package(Git)
+set( BUILD_VERSION 1.1 )
+if( GIT_FOUND )
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --dirty=modified RESULT_VARIABLE GIT_RES OUTPUT_VARIABLE GIT_DESCRIPTION )
+    if( NOT ${GIT_RES} EQUAL 0 )
+        message( WARNING "Git failed (not a repo, or no tags). Defaulting to static build number." )
+    endif()
+    string( REPLACE "\n" "" BUILD_VERSION ${GIT_DESCRIPTION} )
+endif( GIT_FOUND )
+message( STATUS "Version: ${BUILD_VERSION}" )
 
-FILE(WRITE GetVersion.h.txt ${VERSION_STRING} )
-# copy the file to the final header only if the version changes
-# reduces needless rebuilds
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                        GetVersion.h.txt ${CMAKE_CURRENT_BINARY_DIR}/BuildVersion.h)
+set( VERSION_STRING "const static std::string Version = \"${BUILD_VERSION}\"\;\n")
+
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${VERSION_FILE_NAME}.tmp ${VERSION_STRING} )
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_BINARY_DIR}/${VERSION_FILE_NAME}.tmp ${CMAKE_CURRENT_BINARY_DIR}/${VERSION_FILE_NAME} )
