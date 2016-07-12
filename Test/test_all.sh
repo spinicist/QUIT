98c6d115e56aad68511042c8ac4e463c23e7d44f
Tobias Wood <tobias@spinicist.org.uk>
2016-07-12 15:50:44 +0100

    BUG: test_poly not called from test_all

--- Test/test_all.sh
+++ Test/test_all.sh
@@ -6,6 +6,7 @@
 source ./test_common.sh
 
 run_test "DESPOT1" ./test_d1.sh
+run_test "POLY"    ./test_poly.sh
 run_test "FM"      ./test_fm.sh
 run_test "SSFP"    ./test_ssfp.sh
 run_test "RELAX"   ./test_relax.sh
