bec7595d537465ad3d1de551faed340ada3125db
Tobias Wood <tobias@spinicist.org.uk>
2016-07-29 09:59:35 +0100

    ENH: Added ability to write out a transform file.

--- Source/qiaffine.cpp
+++ Source/qiaffine.cpp
@@ -9,88 +9,43 @@
  *
  */
 
-#include <getopt.h>
 #include <iostream>
 
 #include "itkImage.h"
 #include "itkVersor.h"
+#include "itkVersorTransform.h"
+#include "itkTransformFileWriter.h"
 #include "itkChangeInformationImageFilter.h"
 
 #include "QI/Util.h"
 #include "QI/Types.h"
+#include "QI/Option.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    const string usage {
-    "Usage is: qiaffine input [output] [transforms] \n\
-    \n\
-    Applies simple affine transformations to images by manipulating the header\n\
-    transforms. If an output file is not specified, the input file will be\n\
-    overwritten.\n\
-    \n\
-    Transform Options:\n\
-        --scale S : Scale all axes by a factor of S\n\
-        --rotX N  : Rotate about the X axis by N degrees\n\
-        --rotY N  : Rotate about the Y axis by N degrees\n\
-        --rotZ N  : Rotate about the Z axis by N degrees\n\
-        --origin 'X Y Z' : Set the origin to X Y Z\n\
-    \n\
-    Other Options:\n\
-        --help, -h    : Print this message\n\
-        --verbose, -v : Print more messages\n\
-    \n"};
-
-    int verbose = false;
-    const struct option long_options[] =
-    {
-        {"help", no_argument, 0, 'h'},
-        {"verbose", no_argument, 0, 'v'},
-        {"scale", required_argument, 0, 'S'},
-        {"rotX", required_argument, 0, 'X'},
-        {"rotY", required_argument, 0, 'Y'},
-        {"rotZ", required_argument, 0, 'Z'},
-        {"origin", required_argument, 0, 'O'},
-        {0, 0, 0, 0}
-    };
-    const char* short_options = "hv";
-    int c, index_ptr = 0;
-
-    // Do one pass to get options in the right order
-    while ((c = getopt_long(argc, argv, short_options, long_options, &index_ptr)) != -1) {
-        switch (c) {
-            case 'v': verbose = true; break;
-            case 'h':
-                cout << QI::GetVersion() << endl << usage << endl;
-                return EXIT_SUCCESS;
-            case 'S': case 'X': case 'Y': case 'Z': case 'O':
-            case 0: break; // A flag
-            case '?': // getopt will print an error message
-                return EXIT_FAILURE;
-            default:
-            cout << "Unhandled option " << string(1, c) << endl;
-            return EXIT_FAILURE;
-        }
-    }
-
-    if (((argc - optind) == 0) || ((argc - optind) > 2)) {
-        cout << QI::GetVersion() << endl << usage << endl;
-        cout << "Incorrect number of arguments" << endl;
+    Eigen::initParallel();
+    QI::OptionList opts("Usage is: qiaffine input [output] [transforms]\n\nApplies simple affine transformations to images by manipulating the header\ntransforms. If an output file is not specified, the input file will be\noverwritten.");
+    QI::Option<float> scale(1,'\0',"scale","Scale axes by a factor of S", opts);
+    QI::Option<float> rotX(0,'\0',"rotX","Rotate about X axis by N degrees", opts);
+    QI::Option<float> rotY(0,'\0',"rotY","Rotate about X axis by N degrees", opts);
+    QI::Option<float> rotZ(0,'\0',"rotZ","Rotate about X axis by N degrees", opts);
+    QI::Option<std::string> tfmFile("", 't', "tfm","Save ITK transform file", opts);
+    QI::Switch verbose('v',"verbose","Print more information", opts);
+    QI::Help help(opts);
+    std::vector<std::string> nonopts = opts.parse(argc, argv);
+    if ((nonopts.size() == 0) || (nonopts.size() > 2)) {
+        std::cerr << opts << std::endl;
+        std::cerr << "Incorrect number of arguments" << std::endl;
         return EXIT_FAILURE;
     }
 
     // Now read in the input image
     auto reader = itk::ImageFileReader<QI::SeriesF>::New();
-    reader->SetFileName(argv[optind]);
+    reader->SetFileName(nonopts[0]);
     reader->Update();
     auto image = reader->GetOutput();
-    auto writer = itk::ImageFileWriter<QI::SeriesF>::New();
-    if ((argc - optind) == 2) {
-        optind++;
-    }
-    writer->SetFileName(argv[optind]);
 
-    // Now reset optind and actually process
     QI::SeriesF::DirectionType fullDir = image->GetDirection();
     QI::SeriesF::SpacingType fullSpacing = image->GetSpacing();
     QI::SeriesF::PointType fullOrigin = image->GetOrigin();
@@ -104,61 +59,62 @@ int main(int argc, char **argv) {
         origin[i] = fullOrigin[i];
         spacing[i] = fullSpacing[i];
     }
-    optind = 1;
-    while ((c = getopt_long(argc, argv, short_options, long_options, &index_ptr)) != -1) {
-        switch (c) {
-            // Already handled these
-            case 'v': case 'h': case 0: break;
-            case 'S': {
-                const double fac = atof(optarg);
-                if (verbose) cout << "Scaling by factor " << fac << endl;
-                spacing = spacing * fac;
-            } break;
-            case 'X': {
-                if (verbose) cout << "Rotating image by " << string(optarg) << " around X axis." << endl;
-                const double radians = atof(optarg) * vnl_math::pi / 180.0;
-                itk::Versor<double> rotate; rotate.SetRotationAroundX(radians);
-                direction = rotate.GetMatrix() * direction;
-                origin = rotate.GetMatrix() * origin;
-            } break;
-            case 'Y': {
-                if (verbose) cout << "Rotating image by " << string(optarg) << " around Y axis." << endl;
-                const double radians = atof(optarg) * vnl_math::pi / 180.0;
-                itk::Versor<double> rotate; rotate.SetRotationAroundY(radians);
-                direction = rotate.GetMatrix() * direction;
-                origin = rotate.GetMatrix() * origin;
-            } break;
-            case 'Z': {
-                if (verbose) cout << "Rotating image by " << string(optarg) << " around Z axis." << endl;
-                const double radians = atof(optarg) * vnl_math::pi / 180.0;
-                itk::Versor<double> rotate; rotate.SetRotationAroundZ(radians);
-                direction = rotate.GetMatrix() * direction;
-                origin = rotate.GetMatrix() * origin;
-            } break;
-            case 'O': {
-                string option(optarg);
-                if (verbose) cout << "Setting origin to " << option << endl;
-                stringstream optstream(option);
-                optstream >> origin;
-            } break;
+    
+    if (*scale != 1.0) {
+        if (*verbose) cout << "Scaling by factor " << *scale << endl;
+        spacing = spacing * (*scale);
+    }
+    itk::Versor<double> rotate;
+    if (*rotX != 0.0) {
+        if (*verbose) cout << "Rotating image by " << string(optarg) << " around X axis." << endl;
+        rotate.SetRotationAroundX(*rotX * M_PI / 180.0);
+    }
+    if (*rotY != 0.0) {
+        if (*verbose) cout << "Rotating image by " << string(optarg) << " around X axis." << endl;
+        rotate.SetRotationAroundY(*rotY * M_PI / 180.0);
+    }
+    if (*rotZ != 0.0) {
+        if (*verbose) cout << "Rotating image by " << string(optarg) << " around X axis." << endl;
+        rotate.SetRotationAroundZ(*rotZ * M_PI / 180.0);
+    }
+    direction = rotate.GetMatrix() * direction;
+    origin = rotate.GetMatrix() * origin;
+    
+    /*
+        string option(optarg);
+        if (verbose) cout << "Setting origin to " << option << endl;
+        stringstream optstream(option);
+        optstream >> origin;
+    */
+
+    if (*tfmFile != "") { // Only output the transform file
+        auto tfm = itk::VersorTransform<double>::New();
+        tfm->SetRotation(rotate);
+        auto writer = itk::TransformFileWriterTemplate<double>::New();
+        writer->SetInput(tfm);
+        writer->SetFileName(*tfmFile);
+        writer->Update();
+    } else { // Write out the edited file
+        auto changeInfo = itk::ChangeInformationImageFilter<QI::SeriesF>::New();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                fullDir[i][j] = direction[i][j];
+            }
+            fullOrigin[i] = origin[i];
+            fullSpacing[i] = spacing[i];
+        }
+        changeInfo->SetOutputDirection(fullDir);
+        changeInfo->SetOutputOrigin(fullOrigin);
+        changeInfo->SetOutputSpacing(fullSpacing);
+        changeInfo->ChangeDirectionOn();
+        changeInfo->ChangeOriginOn();
+        changeInfo->ChangeSpacingOn();
+        changeInfo->SetInput(image);
+        if (nonopts.size() == 2) {
+            QI::WriteImage(changeInfo->GetOutput(), nonopts[1]);
+        } else {
+            QI::WriteImage(changeInfo->GetOutput(), nonopts[0]);
         }
     }
-    auto changeInfo = itk::ChangeInformationImageFilter<QI::SeriesF>::New();
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            fullDir[i][j] = direction[i][j];
-        }
-        fullOrigin[i] = origin[i];
-        fullSpacing[i] = spacing[i];
-    }
-    changeInfo->SetOutputDirection(fullDir);
-    changeInfo->SetOutputOrigin(fullOrigin);
-    changeInfo->SetOutputSpacing(fullSpacing);
-    changeInfo->ChangeDirectionOn();
-    changeInfo->ChangeOriginOn();
-    changeInfo->ChangeSpacingOn();
-    changeInfo->SetInput(image);
-    writer->SetInput(changeInfo->GetOutput());
-    writer->Update();
     return EXIT_SUCCESS;
 }
