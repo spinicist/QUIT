66b43355319d872248f4633480566dbc403ae5e8
Tobias Wood <tobias@spinicist.org.uk>
2016-10-24 15:23:02 +0100

    ENH: Writes out contrasts

--- Source/qigroups.cpp
+++ Source/qigroups.cpp
@@ -33,11 +33,12 @@ matching line of stdin corresponds to. Group 0 is ignore.\n"
 int main(int argc, char **argv) {
     Eigen::initParallel();
     QI::OptionList opts(usage);
-    QI::Option<std::string> group_path("",'g',"groups","File to read group numbers from", opts);
-    QI::Option<std::string> output_path("",'o',"out","Path for output merged file", opts);
+    QI::Switch sort('s',"sort","Sort groups (and design matrix) in ascending order", opts);
+    QI::Option<std::string> covars_path("",'C',"covars","Path to covariates file (added to design matrix)", opts);
+    QI::Option<std::string> contrasts_path("",'c',"contrasts","Generate and save contrasts", opts);
     QI::Option<std::string> design_path("",'d',"design","Path to save design matrix", opts);
-    QI::Option<std::string> covars_path("",'c',"covars","Path to covariates file (added to design matrix)", opts);
-
+    QI::Option<std::string> output_path("",'o',"out","Path for output merged file", opts);
+    QI::Option<std::string> group_path("",'g',"groups","File to read group numbers from", opts);
     QI::Switch verbose('v',"verbose","Print more information", opts);
     QI::Help help(opts);
     std::vector<std::string> file_paths = opts.parse(argc, argv);
@@ -76,17 +77,10 @@ int main(int argc, char **argv) {
         if (*verbose) std::cout << "Design matrix will be saved to: " << *design_path << std::endl;
         design_file = std::ofstream(*design_path);
     }
-    std::vector<std::string> covars;
+    std::vector<std::vector<std::vector<double>>> covars(n_groups);
+    std::ifstream covars_file;
     if (covars_path.set()) {
-        std::ifstream covars_file(*covars_path);
-        std::string line;
-        while (std::getline(covars_file, line)) {
-            covars.push_back(line);
-        }
-        if (covars.size() != group_list.size()) {
-            std::cerr << "Covariate size does not match group size" << std::endl;
-            return EXIT_FAILURE;
-        }
+        covars_file = std::ifstream(*covars_path);
     }
     int out_index = 0;
     for (int i = 0; i < group_list.size(); i++) {
@@ -95,23 +89,100 @@ int main(int argc, char **argv) {
             if (*verbose) std::cout << "Reading file: " << file_paths.at(i) << " for group " << group << std::endl;
             QI::VolumeF::Pointer ptr = QI::ReadImage(file_paths.at(i));
             groups.at(group - 1).push_back(ptr);
-            tiler->SetInput(out_index, ptr);
-            out_index++;
-            if (design_path.set()) {
-                for (int g = 1; g <= n_groups; g++) {
-                    if (g == group) {
-                        design_file << "1\t";
-                    } else {
-                        design_file << "0\t";
+            std::vector<double> covar;
+            if (covars_path.set()) {
+                if (*verbose) std::cout << "Reading co-variate" << std::endl;
+                std::string covar_line;
+                std::getline(covars_file, covar_line);
+                std::stringstream css(covar_line);
+                double c;
+                while (css >> c) {
+                    covar.push_back(c);
+                }
+                std::cout << "covar " << covar.size() << std::endl; 
+                covars.at(group - 1).push_back(covar);
+            }
+            
+            if (!*sort) {
+                tiler->SetInput(out_index, ptr);
+                out_index++;
+                if (design_path.set()) {
+                    for (int g = 1; g <= n_groups; g++) {
+                        if (g == group) {
+                            design_file << "1\t";
+                        } else {
+                            design_file << "0\t";
+                        }
                     }
+                    if (covars_path.set()) {
+                        for (auto& c : covar) {
+                            design_file << c << "\t";
+                        }
+                    }
+                    design_file << std::endl;
                 }
-                if (covars_path.set()) {
-                    design_file << covars.at(i);
-                }
-                design_file << std::endl;
             }
         } else {
             if (*verbose) std::cout << "Ignoring file: " << file_paths.at(i) << std::endl;
+        }
+    }
+    if (*sort) {
+        if (*verbose) std::cout << "Sorting." << std::endl;
+        for (int g = 0; g < n_groups; g++) {
+            for (int i = 0; i < groups.at(g).size(); i++) {
+                tiler->SetInput(out_index, groups.at(g).at(i));
+                out_index++;
+                if (design_path.set()) {
+                    for (int g2 = 0; g2 < n_groups; g2++) {
+                        if (g2 == g) {
+                            design_file << "1\t";
+                        } else {
+                            design_file << "0\t";
+                        }
+                    }
+                    if (covars_path.set()) {
+                        for (auto& c : covars.at(g).at(i)) {
+                            design_file << c << "\t";
+                        }
+                    }
+                    design_file << std::endl;
+                }
+            }
+        }
+    }
+    if (contrasts_path.set()) {
+        if (*verbose) std::cout << "Generating contrasts" << std::endl;
+        int n_covars = covars_path.set() ? covars.front().front().size() : 0;
+        std::ofstream con_file(*contrasts_path);
+        for (int g = 0; g < n_groups-1; g++) {
+            for (int g2 = 0; g2 < n_groups-1; g2++) {
+                if (g2 == g) {
+                    con_file << "1\t-1\t";
+                } else {
+                    con_file << "0\t";
+                }
+            }
+            for (int c = 0; c < n_covars; c++) {
+                con_file << "0\t";
+            }
+            con_file << std::endl;
+        }
+        for (int c = 0; c < 2*n_covars; c++) {
+            for (int g = 0; g < n_groups; g++) {
+                con_file << "0\t";
+            }
+            for (int c2 = 0; c2 < n_covars; c2++) {
+                if ((c/2) == c2) {
+                    if (c % 2 == 0) {
+                        con_file << "1\t";
+                    } else {
+                        con_file << "-1\t";
+                    }
+                } else {
+                    con_file << "0\t";
+                }
+            }
+            con_file << std::endl;
         }
     }
     if (*verbose) std::cout << "Writing merged file: " << *output_path << std::endl;
