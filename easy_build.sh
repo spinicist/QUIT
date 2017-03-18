1debaa5f4bf88ed22bbd842bd7c7704c7bc03464
Tobias Wood <tobias@spinicist.org.uk>
2017-03-18 23:14:24 +0000

    WIP: Updating build script

--- easy_build.sh
+++ easy_build.sh
@@ -13,50 +13,44 @@
 #
 
 WD=$PWD
-# First download Eigen & ITK
-EXT_DIR="External"
-mkdir -p $EXT_DIR
+
+# First initialize submodules
+EXT_DIR="$WD/External"
 cd $EXT_DIR
+git submodule init && git submodule update
 
-# Eigen
-EIGEN_VER="3.2.4"
-EIGEN_DIR="eigen${EIGEN_VER}"
-EIGEN_URL="http://bitbucket.org/eigen/eigen/get/${EIGEN_VER}.tar.gz"
-curl --location $EIGEN_URL > ${EIGEN_DIR}.tar.gz
-mkdir -p $EIGEN_DIR
-tar --extract --file=${EIGEN_DIR}.tar.gz --strip-components=1 --directory=${EIGEN_DIR}
-rm ${EIGEN_DIR}.tar.gz
-
-# CppSolvers
-SOLVERS_DIR="CppNumericalSolvers"
-SOLVERS_URL="http://github.com/spinicist/CppNumericalSolvers/archive/cmaes_rebase.tar.gz"
-curl --location $SOLVERS_URL > ${SOLVERS_DIR}.tar.gz
-mkdir -p ${SOLVERS_DIR}
-tar --extract --file=${SOLVERS_DIR}.tar.gz --strip-components=1 --directory=${SOLVERS_DIR}
-rm ${SOLVERS_DIR}.tar.gz
+# Eigen is header only
 
 # ITK
-ITK_MAJOR="4.10"
-ITK_MINOR="0"
-ITK_VER="${ITK_MAJOR}.${ITK_MINOR}"
-ITK_SRC_DIR="ITK-${ITK_VER}"
-ITK_BLD_DIR="ITK-${ITK_VER}-Build"
-ITK_URL="https://github.com/InsightSoftwareConsortium/ITK/archive/v${ITK_MAJOR}.${ITK_MINOR}.tar.gz"
-ITK_OPTS="-DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DModule_ITKReview=On -DCMAKE_CXX_STANDARD=11 -DCMAKE_CXX_FLAGS=-fpermissive"
-curl --location ${ITK_URL} > ${ITK_DIR}.tar.gz
-mkdir -p $ITK_SRC_DIR
-tar --extract --file=${ITK_DIR}.tar.gz --strip-components=1 --directory=${ITK_SRC_DIR}
-rm ${ITK_DIR}.tar.gz
-mkdir -p $ITK_BLD_DIR
-cd $ITK_BLD_DIR
-cmake ../${ITK_SRC_DIR} ${ITK_OPTS}
+ITK_DIR="$EXTERNAL/ITK"
+ITK_BUILD_DIR="$ITK_DIR/build"
+ITK_OPTS="-DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF\
+ -DModule_ITKReview=On -DCMAKE_CXX_STANDARD=11 -DCMAKE_CXX_FLAGS=-fpermissive"
+mkdir -p $ITK_BUILD_DIR
+cd $ITK_BUILD_DIR
+cmake $ITK_DIR $ITK_OPTS
 make -j 4
+
+# Ceres
+CERES_DIR="$EXTERNAL/ceres-solver"
+CERES_BUILD_DIR="$CERES_DIR/build"
+CERES_OPTS="-DCMAKE_BUILD_TYPE=Release\
+            -DBUILD_DOCUMENTATION=OFF -DBUILD_EXAMPLES=OFF\
+            -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF\
+            -DCUSTOM_BLAS=OFF -DCXSPARSE=OFF -DCXX11=ON\
+            -DEIGENSPARSE=OFF -DEXPORT_BUILD_DIR=ON\
+            -DGFLAGS=OFF -DLAPACK=OFF -DMINIGLOG=ON\
+            -DOPENMP=OFF -DSUITESPARSE=OFF"
+
+mkdir -p $CERES_BUILD_DIR
+cd $CERES_BUILD_DIR
+cmake $CERES_DIR $CERES_OPTS
 
 cd $WD
 # Now build QUIT
-QUIT_BLD_DIR="Build"
+QUIT_BLD_DIR="$WD/build"
 QUIT_OPTS="-DCMAKE_BUILD_TYPE=Release -DITK_DIR=${WD}/${EXT_DIR}/${ITK_BLD_DIR} -DEIGEN3_INCLUDE_DIR=${WD}/${EXT_DIR}/${EIGEN_DIR}"
 mkdir -p $QUIT_BLD_DIR
 cd $QUIT_BLD_DIR
 cmake $WD ${QUIT_OPTS}
-make -j
+make -j 4
