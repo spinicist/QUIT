20a0b778f3bf9294c2018c8507f65ba1b1c4195d
Tobias Wood <tobias@spinicist.org.uk>
2017-04-19 21:29:03 +0100

    ENH: Now supports different data-types. BUG: Wasn't coping with axes swaps properly.

--- Source/qiaffine.cpp
+++ Source/qiaffine.cpp
@@ -23,37 +23,37 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
-    Eigen::initParallel();
+/*
+ * Declare arguments here so they are available to pipeline
+ */
+args::ArgumentParser parser("Applies simple affine transformations to images by manipulating the header\n"
+    "transforms. If an output file is not specified, the input file will be\n"
+    "overwritten.\n"
+    "http://github.com/spinicist/QUIT");
 
-    args::ArgumentParser parser("Applies simple affine transformations to images by manipulating the header\n"
-        "transforms. If an output file is not specified, the input file will be\n"
-        "overwritten.\n"
-        "http://github.com/spinicist/QUIT");
+args::Positional<std::string> source_path(parser, "SOURCE", "Source file");
+args::Positional<std::string> dest_path(parser, "DEST", "Destination file");
 
-    args::Positional<std::string> source_path(parser, "SOURCE", "Source file");
-    args::Positional<std::string> dest_path(parser, "DEST", "Destination file");
+args::HelpFlag help(parser, "HELP", "Show this help menu", {'h', "help"});
+args::Flag     verbose(parser, "VERBOSE", "Print more information", {'v', "verbose"});
+args::Flag     center(parser, "CENTER", "Set the origin to the center of the image", {'c', "center"});
+args::ValueFlag<std::string> tfm_path(parser, "TFM", "Write out the transformation to a file", {'t', "tfm"});
+args::ValueFlag<double> scale(parser, "SCALE", "Scale by a constant", {'s', "scale"});
+args::ValueFlag<double> offX(parser, "OFF_X", "Translate origin in X direction", {"offX"});
+args::ValueFlag<double> offY(parser, "OFF_Y", "Translate origin in Y direction", {"offY"});
+args::ValueFlag<double> offZ(parser, "OFF_Z", "Translate origin in Z direction", {"offZ"});
+args::ValueFlag<double> rotX(parser, "ROT_X", "Rotate about X-axis by angle (degrees)", {"rotX"});
+args::ValueFlag<double> rotY(parser, "ROT_Y", "Rotate about Y-axis by angle (degrees)", {"rotY"});
+args::ValueFlag<double> rotZ(parser, "ROT_Z", "Rotate about Z-axis by angle (degrees)", {"rotZ"});
 
-    args::HelpFlag help(parser, "HELP", "Show this help menu", {'h', "help"});
-    args::Flag     verbose(parser, "VERBOSE", "Print more information", {'v', "verbose"});
-    args::Flag     center(parser, "CENTER", "Set the origin to the center of the image", {'c', "center"});
-    args::ValueFlag<std::string> tfm_path(parser, "TFM", "Write out the transformation to a file", {'t', "tfm"});
-    args::ValueFlag<double> scale(parser, "SCALE", "Scale by a constant", {'s', "scale"});
-    args::ValueFlag<double> offX(parser, "OFF_X", "Translate origin in X direction", {"offX"});
-    args::ValueFlag<double> offY(parser, "OFF_Y", "Translate origin in Y direction", {"offY"});
-    args::ValueFlag<double> offZ(parser, "OFF_Z", "Translate origin in Z direction", {"offZ"});
-    args::ValueFlag<double> rotX(parser, "ROT_X", "Rotate about X-axis by angle (degrees)", {"rotX"});
-    args::ValueFlag<double> rotY(parser, "ROT_Y", "Rotate about Y-axis by angle (degrees)", {"rotY"});
-    args::ValueFlag<double> rotZ(parser, "ROT_Z", "Rotate about Z-axis by angle (degrees)", {"rotZ"});
+template<typename TImage>
+int Pipeline() {
+    auto image = QI::ReadImage<TImage>(QI::CheckPos(source_path));
 
-    QI::ParseArgs(parser, argc, argv);
-
-    auto image = QI::ReadImage<QI::SeriesF>(QI::CheckPos(source_path));
-
-    QI::SeriesF::DirectionType fullDir = image->GetDirection();
-    QI::SeriesF::SpacingType fullSpacing = image->GetSpacing();
-    QI::SeriesF::PointType fullOrigin = image->GetOrigin();
-    QI::SeriesF::SizeType fullSize = image->GetLargestPossibleRegion().GetSize();
+    typename TImage::DirectionType fullDir = image->GetDirection();
+    typename TImage::SpacingType fullSpacing = image->GetSpacing();
+    typename TImage::PointType fullOrigin = image->GetOrigin();
+    typename TImage::SizeType fullSize = image->GetLargestPossibleRegion().GetSize();
     QI::VolumeF::DirectionType direction;
     QI::VolumeF::SpacingType spacing;
 
@@ -68,14 +68,12 @@ int main(int argc, char **argv) {
         spacing[i] = fullSpacing[i];
         size[i] = fullSize[i];
     }
+
     auto img_tfm = TAffine::New();
-    itk::Versor<double> vd; vd.Set(direction);
-    auto vt = itk::VersorRigid3DTransform<double>::New();
-    vt->SetRotation(vd);
-    img_tfm->Compose(vt);
+    img_tfm->SetMatrix(direction);
     img_tfm->Scale(spacing);
     img_tfm->Translate(origin);
-
+    if (verbose) std::cout << "Start transform:\n" << img_tfm->GetMatrix() << std::endl;
     auto tfm = TAffine::New();
     if (scale) {
         if (verbose) cout << "Scaling by factor " << scale.Get() << endl;
@@ -117,6 +115,7 @@ int main(int argc, char **argv) {
 
     img_tfm->Compose(tfm);
     itk::CenteredAffineTransform<double, 3>::MatrixType fmat = img_tfm->GetMatrix();
+    if (verbose) std::cout << "Final transform:\n" << fmat << std::endl;
     for (int i = 0; i < 3; i++) {
         fullOrigin[i] = img_tfm->GetOffset()[i];
     }
@@ -142,4 +141,36 @@ int main(int argc, char **argv) {
         QI::WriteImage(image, source_path.Get());
     }
     return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+    QI::ParseArgs(parser, argc, argv);
+    if (verbose) std::cout << "Reading header for: " << QI::CheckPos(source_path) << std::endl;
+    auto header = itk::ImageIOFactory::CreateImageIO(source_path.Get().c_str(), itk::ImageIOFactory::ReadMode);
+    header->SetFileName(source_path.Get());
+    header->ReadImageInformation();
+    auto dims  = header->GetNumberOfDimensions();
+    auto dtype = header->GetComponentType();
+    if (verbose) std::cout << "Datatype is " << header->GetComponentTypeAsString( dtype ) << std::endl;
+
+    #define DIM_SWITCH( TYPE ) \
+        switch (dims) { \
+            case 3: return Pipeline<itk::Image< TYPE , 3 >>(); \
+            case 4: return Pipeline<itk::Image< TYPE , 4 >>(); \
+            default: QI_FAIL("Unsupported dimension: " << dims); return EXIT_FAILURE; \
+        }
+
+    switch (dtype) {
+        case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE: QI_FAIL("Unknown component type in image " << source_path);
+        case itk::ImageIOBase::UCHAR:  DIM_SWITCH( unsigned char ); break;
+        case itk::ImageIOBase::CHAR:   DIM_SWITCH( char ); break;
+        case itk::ImageIOBase::USHORT: DIM_SWITCH( unsigned short ); break;
+        case itk::ImageIOBase::SHORT:  DIM_SWITCH( short ); break;
+        case itk::ImageIOBase::UINT:   DIM_SWITCH( unsigned int ); break;
+        case itk::ImageIOBase::INT:    DIM_SWITCH( int ); break;
+        case itk::ImageIOBase::ULONG:  DIM_SWITCH( unsigned long ); break;
+        case itk::ImageIOBase::LONG:   DIM_SWITCH( long ); break;
+        case itk::ImageIOBase::FLOAT:  DIM_SWITCH( float ); break;
+        case itk::ImageIOBase::DOUBLE: DIM_SWITCH( double ); break;
+    }
 }
