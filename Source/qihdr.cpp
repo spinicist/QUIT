dc96ff587cdde28f8341b5a1d94faf5a97736ba2
Tobias Wood <tobias@spinicist.org.uk>
2017-04-25 12:19:43 +0100

    ENH: Switched to args library.

--- Source/qihdr.cpp
+++ Source/qihdr.cpp
@@ -12,107 +12,116 @@
 
 #include <iostream>
 #include "itkMetaDataObject.h"
-#include "QI/Types.h"
-#include "QI/Util.h"
-#include "QI/Option.h"
+#include "QI/IO.h"
+#include "QI/Args.h"
 
-using namespace std;
+args::ArgumentParser parser(
+"Extracts information from image headers.\n"
+"By default, a summary of the header is printed. If any options are specified,"
+"only those parts of the header will be printed. Multiple files can be input"
+"in which case the header info is written for each in order.\n"
+"http://github.com/spinicist/QUIT");
 
-const string usage {
-"Usage is: qihdr [options] input[s] \n\
-\n\
-By default, a summary of the header is printed. If options below are specified,\n\
-only those parts of the header will be printed. Multiple files can be input,\n\
-in which case the header info is written for each in order.\n\
-\n"};
-
+args::PositionalList<std::string> filenames(parser, "FILES", "Input files");
+args::HelpFlag help(parser, "HELP", "Show this help menu", {'h', "help"});
+args::Flag     verbose(parser, "VERBOSE", "Print description of each output line", {'v', "verbose"});
+args::Flag print_direction(parser, "DIRECTION", "Print the image direction/orientation", {'d', "direction"});
+args::Flag print_origin(parser, "ORIGIN", "Print the the origin", {'o', "origin"});
+args::ValueFlag<int> print_spacing(parser, "SPACING", "Print voxel spacing (can specify one dimension)", {'S',"spacing"});
+args::ValueFlag<int> print_size(parser, "SIZE", "Print the matrix size (can specify one dimension)", {'s',"size"});
+args::Flag print_voxvol(parser, "VOLUME", "Calculate and print the volume of one voxel", {'v',"voxvol"});
+args::Flag print_type(parser, "DTYPE", "Print the data type",{'T',"dtype"});
+args::Flag print_dims(parser, "DIMS", "Print the number of dimensions",{'D',"dims"});
+args::Flag dim3(parser, "3D", "Treat input as 3D (discard higher dimensions)", {'3',"3D"});
+args::ValueFlagList<std::string> header_fields(parser, "METADATA", "Print a header metadata field (can be specified multiple times)", {'m', "meta"});
 
 //******************************************************************************
 // Main
 //******************************************************************************
 int main(int argc, char **argv) {
 
-    QI::OptionList opts(usage);
-    QI::Switch print_direction('d', "direction", "Print the image direction/orientation", opts);
-    QI::Switch print_origin('o', "origin", "Print the the origin", opts);
-    QI::Switch print_spacings('S',"spacings", "Print the voxel spacings/sizes", opts);
-    QI::Option<int> print_spacing(0, 'P',"spacing", "Print a specific spacing/size", opts);
-    QI::Option<string> header_field("", 'h', "header", "Print a header field", opts);
-    QI::Switch print_voxvol('v',"voxvol","Calculate and print the volume of one voxel", opts);
-    QI::Switch print_sizes('s',"sizes", "Print the dimension/matrix sizes", opts);
-    QI::Switch print_type('T',"dtype", "Print the data type", opts);
-    QI::Switch print_dims('D',"dims","Print the number of dimensions", opts);
-    QI::Switch dim3('3',"3D","Treat input as 3D (discard higher dimensions)", opts);
-    QI::Help help(opts);
-    std::deque<std::string> nonopts = opts.parse(argc, argv);
-    if (nonopts.size() < 1) {
-        std::cerr << opts << std::endl;
-        std::cerr << "No input filename specified." << std::endl;
-        return EXIT_FAILURE;
-    }
-    bool print_all = !(*print_direction || *print_origin || *print_spacings || *print_spacing ||
-                       *print_sizes || *print_type || *print_dims || *print_voxvol );
-    for (const string& fname : nonopts) {
+    QI::ParseArgs(parser, argc, argv);
+    bool print_all = !(print_direction || print_origin || print_spacing || print_size ||
+                       print_voxvol || print_type || print_dims || header_fields);
+    for (const std::string& fname : QI::CheckList(filenames)) {
         itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImageIO(fname.c_str(), itk::ImageIOFactory::ReadMode);
         if (!imageIO) {
-            cerr << "Could not open: " << string(fname) << endl;
+            std::cerr << "Could not open: " << std::string(fname) << std::endl;
             break;
         }
-        imageIO->SetFileName(string(fname));
+        imageIO->SetFileName(std::string(fname));
         imageIO->ReadImageInformation();
         size_t dims = imageIO->GetNumberOfDimensions();
-        if (print_all) cout << "File:        " << string(fname) << endl;
-        if (print_all) cout << "Dimension:   "; if (print_all || *print_dims) cout << dims << endl;
-        if (print_all) cout << "Voxel Type:  ";
-        if (print_all | *print_type) {
-            cout << imageIO->GetPixelTypeAsString(imageIO->GetPixelType()) << " " 
-                 << imageIO->GetComponentTypeAsString(imageIO->GetComponentType()) << endl;
+        if (verbose) std::cout << "File:       " << std::string(fname) << std::endl;
+        if (print_all || verbose) std::cout << "Dimension:  "; if (print_all || print_dims) std::cout << dims << std::endl;
+        if (print_all || verbose) std::cout << "Voxel Type: ";
+        if (print_all || print_type) {
+            std::cout << imageIO->GetPixelTypeAsString(imageIO->GetPixelType()) << " " 
+                      << imageIO->GetComponentTypeAsString(imageIO->GetComponentType()) << std::endl;
         }
-        if (*dim3 && dims > 3) dims = 3;
-        if (print_all) cout << "Size:         "; if (print_all || *print_sizes)    { for (int i = 0; i < dims; i++) cout << imageIO->GetDimensions(i) << "\t"; cout << endl; }
-        if (print_all) cout << "Voxel volume: "; if (print_all || *print_voxvol)   { double vol = imageIO->GetSpacing(0); for (int i = 1; i < dims; i++) vol *= imageIO->GetSpacing(i); cout << vol << endl; }
-        if (print_all) cout << "Spacing:      "; if (print_all || *print_spacings) { for (int i = 0; i < dims; i++) cout << imageIO->GetSpacing(i) << "\t"; cout << endl; }
-        if (print_all) cout << "Origin:       "; if (print_all || *print_origin)   { for (int i = 0; i < dims; i++) cout << imageIO->GetOrigin(i) << "\t"; cout << endl; }
-        if (print_all) cout << "Direction:    " << endl;
-        if (print_all | *print_direction) {
+        if (dim3 && dims > 3) dims = 3;
+        if (print_all || print_size) {
+            if (print_size.Get() < 0 || print_size.Get() > dims) {
+                if (verbose) std::cerr << "Invalid dimension " << print_size.Get() << " for image " << fname << std::endl;
+            } else {
+                int start_dim, end_dim;
+                if (print_size.Get() == 0) {
+                    start_dim = 0; end_dim = dims;
+                } else {
+                    start_dim = print_size.Get() - 1;
+                    end_dim = print_size.Get();
+                }
+                if (print_all || verbose) std::cout << "Size:       "; if (print_all || print_size) { for (int i = start_dim; i < end_dim; i++) std::cout << imageIO->GetDimensions(i) << "\t"; std::cout << std::endl; }
+            }
+        }
+        if (print_all || print_spacing) {
+            if (print_spacing.Get() < 0 || print_spacing.Get() > dims) {
+                if (verbose) std::cerr << "Invalid dimension " << print_spacing.Get() << " for image " << fname << std::endl;
+            } else {
+                int start_dim, end_dim;
+                if (print_spacing.Get() == 0) {
+                    start_dim = 0; end_dim = dims;
+                } else {
+                    start_dim = print_spacing.Get() - 1;
+                    end_dim = print_spacing.Get();
+                }
+                if (print_all || verbose) std::cout << "Spacing:    "; if (print_all || print_spacing) { for (int i = start_dim; i < end_dim; i++) std::cout << imageIO->GetSpacing(i) << "\t"; std::cout << std::endl; }
+            }
+        }
+        if (print_all || verbose) std::cout << "Origin:     "; if (print_all || print_origin)   { for (int i = 0; i < dims; i++) std::cout << imageIO->GetOrigin(i) << "\t"; std::cout << std::endl; }
+        if (print_all || verbose) std::cout << "Direction:  " << std::endl;
+        if (print_all | print_direction) {
             for (int i = 0; i < dims; i++) {
                 std::vector<double> dir = imageIO->GetDirection(i);
                 for (int j = 0; j < dims; j++)
-                    cout << dir[j] << "\t";
-                cout << endl;
+                    std::cout << dir[j] << "\t";
+                std::cout << std::endl;
             }
         }
-        if (print_spacing.set()) {
-
-            if (*print_spacing > -1 && *print_spacing < dims) {
-                cout << imageIO->GetSpacing(*print_spacing) << endl;
-            } else {
-                cerr << "Invalid dimension " << *print_spacing << " for image " << fname << endl;
-            }
-        }
-        if (header_field.set()) {
+        if (print_all || verbose) std::cout << "Voxel vol:  "; if (print_all || print_voxvol)   { double vol = imageIO->GetSpacing(0); for (int i = 1; i < dims; i++) vol *= imageIO->GetSpacing(i); std::cout << vol << std::endl; }
+        for (const std::string &hf : header_fields.Get()) {
             auto header = imageIO->GetMetaDataDictionary();
-            if (header.HasKey(*header_field)) {
+            if (header.HasKey(hf)) {
                 std::vector<std::string> string_array_value;
                 std::vector<double> double_array_value;
                 std::string string_value;
                 double double_value;
-                std::cout << *header_field << ": ";
-                if (ExposeMetaData(header, *header_field, string_array_value)) {
+                if (verbose) std::cout << hf << ": ";
+                if (ExposeMetaData(header, hf, string_array_value)) {
                     for (const auto &v : string_array_value) std::cout << v << ",";
                     std::cout << std::endl;
-                } else if (ExposeMetaData(header, *header_field, double_array_value)) {
+                } else if (ExposeMetaData(header, hf, double_array_value)) {
                     for (const auto &v : double_array_value) std::cout << v << ",";
                     std::cout << std::endl;
-                } else if (ExposeMetaData(header, *header_field, string_value)) {
+                } else if (ExposeMetaData(header, hf, string_value)) {
                    std::cout << string_value << std::endl;
-                } else if (ExposeMetaData(header, *header_field, double_value)) {
+                } else if (ExposeMetaData(header, hf, double_value)) {
                     std::cout << double_value << std::endl;
                 } else {
-                    QI_EXCEPTION("Could not determine type of rename header field:" << *header_field);
+                    QI_EXCEPTION("Could not determine type of rename header field:" << hf);
                 }
             } else {
-                std::cout << "Header field not found: " << *header_field << std::endl;
+                if (verbose) std::cout << "Header field not found: " << hf << std::endl;
             }
         }
     }
