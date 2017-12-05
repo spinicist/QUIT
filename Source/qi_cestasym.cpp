ca1a257130d947cd5da39c62560924baba430684
Tobias Wood <tobias@spinicist.org.uk>
2017-12-05 14:20:22 +0000

    ENH: Incorporated f0 map

--- Source/qi_cestasym.cpp
+++ Source/qi_cestasym.cpp
@@ -29,14 +29,14 @@ public:
         m_zero = TOutput(m_afrqs.rows()); m_zero.Fill(0.);
     }
     size_t numInputs() const override { return 1; }
-    size_t numConsts() const override { return 0; }
+    size_t numConsts() const override { return 1; }
     size_t numOutputs() const override { return 1; }
     size_t dataSize() const override { return m_zfrqs.rows(); }
     size_t outputSize() const override {
         return m_afrqs.rows();
     }
     std::vector<float> defaultConsts() const override {
-        std::vector<float> def(0, 1.0f);
+        std::vector<float> def(1, 0.0f);
         return def;
     }
     TOutput zero() const override {
@@ -52,10 +52,11 @@ public:
     {
         const Eigen::Map<const Eigen::ArrayXf> zdata(inputs[0].GetDataPointer(), m_zfrqs.rows());
         QI::SplineInterpolator zspec(m_zfrqs, zdata.cast<double>());
-        float ref = zdata[0];
+        double ref = zdata[0];
+        double f0 = consts.at(0);
         for (int f = 0; f < m_afrqs.rows(); f++) {
-            const double pfrq = m_afrqs[f];
-            const double nfrq = -pfrq;
+            const double pfrq = m_afrqs[f] - f0;
+            const double nfrq = f0 - m_afrqs[f];
             const double pos = zspec(pfrq);
             const double neg = zspec(nfrq);
             outputs.at(0)[f] = ((pos - neg)/ref);
@@ -76,6 +77,7 @@ int main(int argc, char **argv) {
     args::ValueFlag<int> threads(parser, "THREADS", "Use N threads (default=4, 0=hardware limit)", {'T', "threads"}, 4);
     args::ValueFlag<std::string> outarg(parser, "PREFIX", "Add a prefix to output filenames", {'o', "out"});
     args::ValueFlag<std::string> mask(parser, "MASK", "Only process voxels within the mask", {'m', "mask"});
+    args::ValueFlag<std::string> f0(parser, "OFF RESONANCE", "Specify off-resonance frequency", {'f', "f0"});
     args::ValueFlag<std::string> subregion(parser, "REGION", "Process subregion starting at voxel I,J,K with size SI,SJ,SK", {'s', "subregion"});
     QI::ParseArgs(parser, argc, argv);
     bool prompt = !noprompt;
@@ -92,7 +94,14 @@ int main(int argc, char **argv) {
     apply->SetAlgorithm(algo);
     apply->SetPoolsize(threads.Get());
     apply->SetInput(0, data);
-    if (mask) apply->SetMask(QI::ReadImage(mask.Get()));
+    if (mask) {
+        if (verbose) std::cout << "Setting mask image: " << mask.Get() << std::endl;
+        apply->SetMask(QI::ReadImage(mask.Get()));
+    }
+    if (f0) {
+        if (verbose) std::cout << "Setting f0 image: " << f0.Get() << std::endl;
+        apply->SetConst(0, QI::ReadImage(f0.Get()));
+    }
     if (subregion) {
         apply->SetSubregion(QI::RegionArg(subregion.Get()));
     }
