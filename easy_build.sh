f735f57003dc03036c0c05a6f4a832f753daa231
Tobias Wood <tobias@spinicist.org.uk>
2018-03-26 11:36:46 +0100

    ENH: Allowed ninja in easy_build

--- easy_build.sh
+++ easy_build.sh
@@ -13,7 +13,7 @@
 #
 
 WD=$PWD
-# First download Eigen & ITK
+# Initialise submodules
 git submodule update --init
 EXTERNAL="$WD/External"
 cd $EXTERNAL
@@ -23,10 +23,19 @@ EIGEN_DIR="$EXTERNAL/eigen"
 ARGS_DIR="$EXTERNAL/args"
 CEREAL_DIR="$EXTERNAL/cereal/include"
 
+# Check for presence of ninja
+if [ -x "$(which ninja)" ]; then
+    GENERATOR="-GNinja"
+    BUILDCMD="ninja"
+else
+    GENERATOR=""
+    BUILDCMD="make -j 2"
+fi
+
 # Ceres
 CERES_DIR="$EXTERNAL/ceres-solver"
 CERES_BUILD_DIR="$CERES_DIR/build"
-CERES_OPTS="-DCMAKE_BUILD_TYPE=Release\
+CERES_OPTS="$GENERATOR -DCMAKE_BUILD_TYPE=Release\
             -DBUILD_DOCUMENTATION=OFF -DBUILD_EXAMPLES=OFF\
             -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF\
             -DCUSTOM_BLAS=OFF -DCXSPARSE=OFF -DCXX11=ON\
@@ -34,22 +43,21 @@ CERES_OPTS="-DCMAKE_BUILD_TYPE=Release\
             -DEXPORT_BUILD_DIR=ON\
             -DGFLAGS=OFF -DLAPACK=OFF -DMINIGLOG=ON\
             -DOPENMP=OFF -DSUITESPARSE=OFF"
-
 mkdir -p $CERES_BUILD_DIR
 cd $CERES_BUILD_DIR
 cmake $CERES_DIR $CERES_OPTS
-make -j 2
+$BUILDCMD
 
 # ITK
 ITK_DIR="$EXTERNAL/ITK"
 ITK_BUILD_DIR="$ITK_DIR/build"
-ITK_OPTS="-DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF\
-           -DBUILD_TESTING=OFF -DModule_ITKReview=On\
+ITK_OPTS="$GENERATOR -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF\
+           -DBUILD_TESTING=OFF -DITK_BUILD_DEFAULT_MODULES=OFF -DModule_ITKReview=On\
            -DCMAKE_CXX_STANDARD=11 -DCMAKE_CXX_FLAGS=-fpermissive"
 mkdir -p $ITK_BUILD_DIR
 cd $ITK_BUILD_DIR
 cmake $ITK_DIR $ITK_OPTS
-make -j 2
+$BUILDCMD
 
 cd $WD
 # Now build QUIT
@@ -61,11 +69,11 @@ else
     QUIT_INSTALL_DIR=""
 fi
 QUIT_BLD_DIR="build"
-QUIT_OPTS="-DCMAKE_BUILD_TYPE=Release\
+QUIT_OPTS="$GENERATOR -DCMAKE_BUILD_TYPE=Release\
     -DEIGEN3_INCLUDE_DIR=$EIGEN_DIR -DCeres_DIR=$CERES_BUILD_DIR\
     -DITK_DIR=$ITK_BUILD_DIR -DArgs_DIR=$ARGS_DIR -DCereal_DIR=$CEREAL_DIR
     $QUIT_INSTALL_DIR"
 mkdir -p $QUIT_BLD_DIR
 cd $QUIT_BLD_DIR
 cmake $WD $QUIT_OPTS
-make -j 2
+$BUILDCMD
