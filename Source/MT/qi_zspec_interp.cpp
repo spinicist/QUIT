4d733086cf1eb7319d561a5b90837d1475ef8f64
Tobias Wood <tobias@spinicist.org.uk>
2018-07-26 11:21:46 +0100

    ENH: Refactored mtasym to interpolate Z-spectra and use JSON

--- Source/MT/qi_zspec_interp.cpp
+++ Source/MT/qi_zspec_interp.cpp
@@ -15,30 +15,30 @@
 #include "Util.h"
 #include "Args.h"
 #include "ImageIO.h"
-#include "IO.h"
+#include "JSON.h"
 #include "Spline.h"
 #include "ApplyTypes.h"
 
-class MTAsym : public QI::ApplyVectorF::Algorithm {
+class InterpZSpec : public QI::ApplyVectorF::Algorithm {
 protected:
-    Eigen::ArrayXd m_zfrqs, m_afrqs;
+    Eigen::ArrayXd m_ifrqs, m_ofrqs;
     TOutput m_zero;
     
 public:
-    MTAsym(const Eigen::ArrayXf &zf, const Eigen::ArrayXf &af) :
-        m_zfrqs(zf.cast<double>()), m_afrqs(af.cast<double>())
+    InterpZSpec(const Eigen::ArrayXd &z, const Eigen::ArrayXd &a) :
+        m_ifrqs(z), m_ofrqs(a)
     {
-        m_zero = TOutput(m_afrqs.rows()); m_zero.Fill(0.);
+        m_zero = TOutput(m_ofrqs.rows()); m_zero.Fill(0.);
     }
     size_t numInputs() const override { return 1; }
     size_t numConsts() const override { return 1; }
     size_t numOutputs() const override { return 1; }
-    size_t dataSize() const override { return m_zfrqs.rows(); }
+    size_t dataSize() const override { return m_ifrqs.rows(); }
     size_t outputSize() const override {
-        return m_afrqs.rows();
+        return m_ofrqs.rows();
     }
     std::vector<float> defaultConsts() const override {
-        std::vector<float> def(1, 0.0f);
+        std::vector<float> def{{0.0f}};
         return def;
     }
     TOutput zero() const override {
@@ -53,44 +53,39 @@ public:
                std::vector<TOutput> &outputs, TOutput & /* Unused */,
                TInput & /* Unused */, TIterations & /* Unused */) const override
     {
-        const Eigen::Map<const Eigen::ArrayXf> zdata(inputs[0].GetDataPointer(), m_zfrqs.rows());
-        QI::SplineInterpolator zspec(m_zfrqs, zdata.cast<double>());
-        double ref = zdata[0];
+        const Eigen::Map<const Eigen::ArrayXf> zdata(inputs[0].GetDataPointer(), m_ifrqs.rows());
+        QI::SplineInterpolator zspec(m_ifrqs, zdata.cast<double>());
         double f0 = consts.at(0);
-        for (int f = 0; f < m_afrqs.rows(); f++) {
-            const double pfrq = m_afrqs[f] - f0;
-            const double nfrq = f0 - m_afrqs[f];
-            const double pos = zspec(pfrq);
-            const double neg = zspec(nfrq);
-            outputs.at(0)[f] = ((pos - neg)/ref);
+        for (int f = 0; f < m_ofrqs.rows(); f++) {
+            const double frq = m_ofrqs[f] + f0;
+            outputs.at(0)[f] = zspec(frq);
         }
         return std::make_tuple(true, "");
     }
 };
 
-
 int main(int argc, char **argv) {
     Eigen::initParallel();
-    args::ArgumentParser parser("Simple MT-asymmetry calculator.\nhttp://github.com/spinicist/QUIT");
+    args::ArgumentParser parser("Interpolates Z-spectrums using Cubic Splines\nhttp://github.com/spinicist/QUIT");
     
     args::Positional<std::string> input_path(parser, "INPUT", "Input Z-spectrum file");
     args::HelpFlag help(parser, "HELP", "Show this help menu", {'h', "help"});
     args::Flag     verbose(parser, "VERBOSE", "Print more information", {'v', "verbose"});
     args::ValueFlag<int> threads(parser, "THREADS", "Use N threads (default=4, 0=hardware limit)", {'T', "threads"}, QI::GetDefaultThreads());
-    args::ValueFlag<std::string> outarg(parser, "PREFIX", "Add a prefix to output filenames", {'o', "out"});
+    args::ValueFlag<std::string> outarg(parser, "OUTPUT", "Change ouput filename (default is input_interp)", {'o', "out"});
     args::ValueFlag<std::string> mask(parser, "MASK", "Only process voxels within the mask", {'m', "mask"});
-    args::ValueFlag<std::string> f0(parser, "OFF RESONANCE", "Specify off-resonance frequency", {'f', "f0"});
+    args::ValueFlag<std::string> f0(parser, "OFF RESONANCE", "Specify off-resonance frequency (units must match input)", {'f', "f0"});
     args::ValueFlag<std::string> subregion(parser, "REGION", "Process subregion starting at voxel I,J,K with size SI,SJ,SK", {'s', "subregion"});
     QI::ParseArgs(parser, argc, argv, verbose, threads);
 
     QI_LOG(verbose, "Opening file: " << QI::CheckPos(input_path));
     auto data = QI::ReadVectorImage<float>(QI::CheckPos(input_path));
 
-    QI_LOG(verbose, "Enter Z-Spectrum Frequencies: " );
-    Eigen::ArrayXf z_frqs; QI::ReadArray(std::cin, z_frqs);
-    QI_LOG(verbose, "Enter Asymmetry Frequencies: " );
-    Eigen::ArrayXf a_frqs; QI::ReadArray(std::cin, a_frqs); // Asymmetry output
-    std::shared_ptr<MTAsym> algo = std::make_shared<MTAsym>(z_frqs, a_frqs);
+    QI_LOG(verbose, "Reading input frequences");
+    rapidjson::Document json = QI::ReadJSON(std::cin);
+    auto i_frqs = QI::ArrayFromJSON(json["input_freqs"]);
+    auto o_frqs = QI::ArrayFromJSON(json["output_freqs"]);
+    std::shared_ptr<InterpZSpec> algo = std::make_shared<InterpZSpec>(i_frqs, o_frqs);
     auto apply = QI::ApplyVectorF::New();
     apply->SetAlgorithm(algo);
     apply->SetInput(0, data);
@@ -111,13 +106,10 @@ int main(int argc, char **argv) {
         apply->AddObserver(itk::ProgressEvent(), monitor);
     }
     apply->Update();
-    QI_LOG(verbose, "Elapsed time was " << apply->GetTotalTime() << "s" <<
-                    "Writing output.");
-    std::string outPrefix = outarg.Get() + "MT_";
-    for (size_t i = 0; i < algo->numOutputs(); i++) {
-        QI::WriteVectorImage(apply->GetOutput(i), outPrefix + algo->names().at(i) + QI::OutExt());
-    }
-    
+    QI_LOG(verbose, "Elapsed time was " << apply->GetTotalTime() << "s\n");
+    std::string outname = outarg ? outarg.Get() : QI::StripExt(input_path.Get()) + "_interp" + QI::OutExt();
+    QI_LOG(verbose, "Writing output: " << outname);
+    QI::WriteVectorImage(apply->GetOutput(0), outname);
     QI_LOG(verbose, "Finished.");
     return EXIT_SUCCESS;
 }
