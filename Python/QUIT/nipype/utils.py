44b0b3cff964126f9eb8ea88154ce3fbe767c1b3
Emil Ljungberg <emil.ljungberg@kcl.ac.uk>
2018-08-10 11:41:26 +0100

    Implemented qimask and qiaffine

--- Python/QUIT/nipype/utils.py
+++ Python/QUIT/nipype/utils.py
@@ -4,12 +4,10 @@
 """
 Implementation of nipype interfaces for QUIT utilities.
 
-Currently implemented:
-    - qisignal
+Implemented:
+    - qi_coil_combine
 
 To be implemented:
-    - qi_coil_combine
-    - qi_gradient
     - qi_rfprofile
     - qiaffine
     - qicomplex
@@ -18,10 +16,6 @@ To be implemented:
     - qimask
     - qipolyfit
     - qipolyimg
-    - qisplitsubjects
-
-To be moved:
-    - qisignal should now be in CoreProgs
 
 Requires that the QUIT tools are in your your system path
 
@@ -31,94 +25,171 @@ from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 
 from nipype.interfaces.base import CommandLineInputSpec, CommandLine, TraitedSpec, File, traits, isdefined
+from quit_nipype_utils import parse_param_dict
+from QUIT.nipype.base import QUITCommand, QUITCommandInputSpec
+
 import json
+import os
 
-############################ qisignal ############################
-
-class QiSignalInputSpec(CommandLineInputSpec):
+############################ qi_rfprofile ############################
+class QiRFprofileInputSpec(QUITCommandInputSpec):
     
     # Inputs
-    param_file = File(desc='Signal Equation .json file', position=-1, argstr='< %s', 
+    in_file = File(exists=True, argstr='%s', mandatory=True,
+        position=0, desc='Input B1+ file')
+    param_file = File(desc='Parameter .json file', position=2, argstr='< %s', 
         xor=['param_dict'], mandatory=True, exists=True)
-
-    param_dict = traits.Dict(desc='Signal Equation Dict', position=-1, 
+    param_dict = traits.Dict(desc='Parameter dictionary', position=2, 
         argstr='', mandatory=True, xor=['param_file'])
-        
-    out_file = File(exists=False, argstr='%s', mandatory=True,
-        position=-2, desc='Output file')
-    
+
+    # Outputs
+    out_file = File(exists=False, argstr='%s', mandatory=True, desc='Input B1+ file', position=1)
+
     # Options
-    verbose = traits.Bool(desc='Print more information', argstr='-v')
+    mask_file = File(desc='Only process voxels within the mask', argstr='--mask=%s')
+
+    # Commonly used options
+    debug = traits.Bool(desc='Output debugging messages', argstr='-v')
     threads = traits.Int(desc='Use N threads (default=4, 0=hardware limit)', argstr='--threads=%d')
     prefix = traits.String(desc='Add a prefix to output filenames', argstr='--out=%s')
-    mask_file = File(desc='Only process voxels within the mask', argstr='--mask=%s')
-    ref_file = File(desc='Resample inputs to this reference space', argstr='--ref=%s')
-    sim_noise = traits.Float(desc='Add complex noise with std=value', argstr='---noise=%f')
-    sim_seed = traits.Int(desc='Seed noise RNG with specific value', argstr='--seed=%d')
-    n_comp = traits.Int(desc='Choose number of components in model 1/2/3, default 1', argstr='--model=%d')
-    complex_output = traits.Bool(desc='Save complex images', argstr='--complex')
-    environ = {'QUIT_EXT':'NIFTI_GZ'}
+
+class QiRFprofileOutputSpec(TraitedSpec):
+    # Specify which outputs there are
+    out_file = File(desc="output relative B1 file")
     
-class QiSignalOutputSpec(TraitedSpec):
-    output_img = File(desc="Simulated Image")
-
-class QiSignal(CommandLine):
+class QiRFprofile(QUITCommand):
     """
-    Run signal simulation with qisignal
+    help for myInterface
 
-    Example with parameter file
+    Example 1
     -------
-    >>> from QUIT.nipype.utilities import QiSignal
-    >>> qisignal = QiSignal(out_file='test.nii', param_file='seq_params.json')
-    >>> sim_res = d1.run()
-    >>> print(sim_res.outputs)
-
-    Example with parameter dictionary
-    -------
-    >>> from QUIT.nipype.relaxometry import QiDespot1
-    >>> params = {'SPGR': {'TR':5E-3, 'FA':[5,10]} }
-    >>> d = {
-            "PD": "pd.nii",
-            "T1": "t1.nii.gz",
-            "T2": "",
-            "f0": "",
-            "B1": "",
-            "SequenceGroup": {
-                "sequences": [
-                    {
-                        "SPGR": {
-                            "TR": 0.05,
-                            "FA": [3, 10]
-                            }
-                        }
-                    ]
-                }
-            }
-    >>> qisignal = QiSignal(out_file='test.nii', param_dict=d)
-    >>> sim_res = d1.run()
-    >>> print(sim_res.outputs)
-
+    >>> from QUIT.nipype.utils import QiRFprofile
+    >>> interface = QiRFprofile()
+    
     """
 
-    _cmd = 'qisignal'
-    input_spec = QiSignalInputSpec
-    output_spec = QiSignalOutputSpec
+    _cmd = 'qi_rfprofile'
+    input_spec = QiRFprofileInputSpec
+    output_spec = QiRFprofileOutputSpec
 
     def _format_arg(self, name, spec, value):
-        if name == 'param_dict':
-            with open('_tmp_input.json', 'w') as outfile:
-                json.dump(value, outfile)
-            return "< _tmp_input.json"
-
-        return super(QiSignal, self)._format_arg(name, spec, value)
+        return self._process_params(name, spec, value)
 
     def _list_outputs(self):
         outputs = self.output_spec().get()
         
-        prefix = ''
-        if self.inputs.prefix:
-            prefix = self.inputs.prefix
-            
-        outputs['output_img'] = prefix + self.inputs.out_file
+        outputs['out_file'] = os.path.abspath(self._add_prefix(self.inputs.out_file))
         
+        return outputs
+
+############################ qiaffine ############################
+class QiAffineInputSpec(QUITCommandInputSpec):
+    
+    # Inputs
+    in_file = File(exists=True, argstr='%s', mandatory=True,
+        position=0, desc='Source File')
+
+    # Outputs
+    out_file = File(exists=False, argstr='%s', mandatory=True,
+        position=1, desc='Destination File')
+
+    # Options
+    xfm_file = File(exists=False, argstr='--tfm=%s', mandatory=True, desc='Write out the transformation to a file')
+    offX = traits.Int(desc='Translate origin in X direction', argstr='-offX=%d')
+    offY = traits.Int(desc='Translate origin in Y direction', argstr='-offY=%d')
+    offZ = traits.Int(desc='Translate origin in Z direction', argstr='-offZ=%d')
+    rotX = traits.Int(desc='Rotate about X-axis by angle (degrees)', argstr='-rotX=%d')
+    rotY = traits.Int(desc='Rotate about Y-axis by angle (degrees)', argstr='-rotY=%d')
+    rotZ = traits.Int(desc='Rotate about Z-axis by angle (degrees)', argstr='-rotZ=%d')
+    scale = traits.Float(desc='Scale by a constant', argstr='--scale=%f')
+    permute = traits.String(desc='Permute axes, e.g. 2,0,1. Negative values mean flip as well', argstr='--permute=%s')
+    flip = traits.String(desc='Flip an axis, e.g. 0,1,0. Occurs AFTER any permutation.', argstr='--flip=%s')
+
+    # This should probably be enum instead
+    center = traits.String(desc='Set the origin to geometric center (geo) or (cog)', argstr='--center=%s')
+
+class QiAffineOutputSpec(TraitedSpec):
+    # Specify which outputs there are
+    out_file = File(desc="output relative B1 file")
+    
+class QiAffine(QUITCommand):
+    """
+    help for myInterface
+
+    Example 1
+    -------
+    >>> from QUIT.nipype.utils import QiRFprofile
+    >>> interface = QiRFprofile()
+    
+    """
+
+    _cmd = 'qi_rfprofile'
+    input_spec = QiRFprofileInputSpec
+    output_spec = QiRFprofileOutputSpec
+
+    def _format_arg(self, name, spec, value):
+        return self._process_params(name, spec, value)
+
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_file'] = os.path.abspath(self._add_prefix(self.inputs.out_file))        
+        return outputs
+
+############################ qimask ############################
+class QiMaskInputSpec(QUITCommandInputSpec): 
+    # Inputs
+    in_file = File(exists=True, argstr='%s', mandatory=True, position=0, desc='Input File')
+
+    # Outputs
+    out_file = File(exists=False, argstr='%s', mandatory=True,
+                    position=1, desc='Set output filename, default is input + _mask')
+    
+    # Options
+    vol = traits.Int(desc='Choose volume to mask in multi-volume file. Default 1, -1 selects last volume', 
+                    argstr='--volume=%d')
+    complex_data = traits.Bool(desc='Input data is complex, take magnitude first', argstr='--complex')
+    lower = traits.Float(desc="Specify lower intensity threshold for 1st stage, otherwise Otsu's method is used",
+                    argstr='--lower=%f')
+    upper = traits.Float(desc="Specify upper intensity threshold for 1st stage, otherwise Otsu's method is used",
+                    argstr='--upper=%f')
+    rats = traits.Float(desc="Perform the RATS step, argument is size threshold for connected component",
+                    argstr='--rats=%f')
+    fill_holes = traits.Int(desc="Fill holes in thresholded mask with radius N", argstr='--fillh=%d')
+
+class QiMaskOutputSpec(TraitedSpec):
+    # Specify which outputs there are
+    mask_file = File(desc="Output mask file")
+    
+class QiMask(CommandLine):
+    """
+    help for myInterface
+
+    Example 1
+    -------
+    >>> from QUIT.nipype.utils import QiRFprofile
+    >>> interface = QiRFprofile()
+    
+    """
+
+    _cmd = 'qimask'
+    input_spec = QiMaskInputSpec
+    output_spec = QiMaskOutputSpec
+
+    def _format_arg(self, name, spec, value):
+        return self._process_params(name, spec, value)
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        
+        # Specify output files
+        if self.inputs.out_file:
+            output_fname = self.inputs.out_file
+        else:
+            input_bname = os.path.basename(self.inputs.in_file)
+            input_fname = os.path.splitext(os.path.split(input_bname)[-1])
+            output_fname = input_fname + '_mask.nii.gz'
+
+        outputs['mask_file'] = os.path.abspath(output_fname)        
+
         return outputs
