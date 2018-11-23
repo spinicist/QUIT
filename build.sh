014920466023e0b706af9bc64548965851ceae31
Tobias Wood <tobias@mi3.com>
2018-11-23 14:05:18 +0000

    BUG: Updated ITK to master to re-enable C++17

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ INSTALL=""          # Install as well as build
 QUIT_INSTALL_DIR="" # User can specify install directory (mainly for Travis)
 NUM_THREADS="2"     # Specify number of threads during make/ninja
 NATIVE=""           # Specifieds march=native
-while getopts "cij:np:" opt; do
+while getopts "cgij:np:" opt; do
     case $opt in
         c) CERES="";;
         g) CHECKOUT="";;
@@ -75,7 +75,7 @@ fi
 # ITK
 ITK_DIR="$EXTERNAL/ITK"
 ITK_BUILD_DIR="$ITK_DIR/$BUILD_DIR"
-ITK_OPTS="$GENERATOR -DCMAKE_BUILD_TYPE=Release $NATIVE -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS=-fpermissive
+ITK_OPTS="$GENERATOR -DCMAKE_BUILD_TYPE=Release $NATIVE -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS=-fpermissive
            -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DGDCM_USE_COREFOUNDATION_LIBRARY=OFF \
            -DITK_BUILD_DEFAULT_MODULES=OFF\
            -DITKGroup_Core=OFF -DModule_ITKCommon=ON\
