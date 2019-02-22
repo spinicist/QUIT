aaa6f24a28db5702e6a8bafdbd82c0f07b8b60d5
Tobias Wood <tobias@spinicist.org.uk>
2019-02-22 15:54:40 +0000

    BUG Log colors

--- Source/Core/Log.h
+++ Source/Core/Log.h
@@ -23,7 +23,7 @@ namespace QI {
 template <typename S, typename... Args>
 inline void Log(const bool verbose, const S &fmt_str, const Args &... args) {
     if (verbose) {
-        fmt::print(stderr, fmt::fg(fmt::color::white), fmt_str, args...);
+        fmt::print(stderr, fmt::fg(fmt::terminal_color::bright_white), fmt_str, args...);
         fmt::print(stderr, "\n");
     }
 }
@@ -32,16 +32,16 @@ template <typename S, typename... Args>
 inline void Info(const bool verbose, const S &fmt_str, const Args &... args) {
     if (verbose) {
         const std::time_t now = std::time(nullptr);
-        fmt::print(stderr, fmt::fg(fmt::color::green), "{:%T}", *std::localtime(&now));
-        fmt::print(stderr, fmt::fg(fmt::color::white), fmt_str, args...);
+        fmt::print(stderr, fmt::fg(fmt::color::green), "{:%T} ", *std::localtime(&now));
+        fmt::print(stderr, fmt::fg(fmt::terminal_color::bright_white), fmt_str, args...);
         fmt::print(stderr, "\n");
     }
 }
 
 template <typename S, typename... Args> inline void Warn(const S &fmt_str, const Args &... args) {
     const std::time_t now = std::time(nullptr);
-    fmt::print(stderr, fmt::fg(fmt::color::orange), "{:%T}", *std::localtime(&now));
-    fmt::print(stderr, fmt::fg(fmt::color::white), fmt_str, args...);
+    fmt::print(stderr, fmt::fg(fmt::color::orange), "{:%T} ", *std::localtime(&now));
+    fmt::print(stderr, fmt::fg(fmt::terminal_color::bright_white), fmt_str, args...);
     fmt::print(stderr, "\n");
 }
 
