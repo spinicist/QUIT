aaa2d37496cacb7a3b7919e96bed886ad7b5c172
E B <emmabluemke@gmail.com>
2020-06-21 00:41:59 +0100

    removed my own build folder

--- build/Source/Core/CMakeFiles/qi_version.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/qi_version"
-  "version_depend_dummy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qi_version.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
