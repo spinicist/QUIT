dd506bd47f1d0df074c5e37604f42a56792f8bf7
spinicist <tobias@spinicist.org.uk>
2020-07-03 12:09:04 +0100

    ENH Group commands

--- Source/qi_main.cpp
+++ Source/qi_main.cpp
@@ -4,79 +4,87 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    args::ArgumentParser parser("QUIT http://github.com/spinicist/QUIT");
-    args::Group          commands(parser, "COMMANDS");
+    args::ArgumentParser parser("http://github.com/spinicist/QUIT");
     args::GlobalOptions  globals(parser, global_group);
-    args::Group          top(parser, "TOP");
-    args::Flag           version(top, "VERSION", "Print the version of QUIT", {"version"});
 
-#define ADD(CMD, HELP) args::Command CMD(commands, #CMD, HELP, &CMD##_main);
-    ADD(newimage, "Create a new image");
-    ADD(diff, "Calcualte the difference between two images");
-    ADD(hdr, "Print header information from an image");
+#define ADD(CMD, GROUP, HELP) args::Command CMD(GROUP, #CMD, HELP, &CMD##_main);
+
+    args::Group core(parser, "CORE");
+    args::Flag  version(core, "VERSION", "Print the version of QUIT", {"version"});
+    ADD(newimage, core, "Create a new image");
+    ADD(diff, core, "Calcualte the difference between two images");
+    ADD(hdr, core, "Print header information from an image");
 #ifdef BUILD_B1
-    ADD(afi, "Actual Flip-Angle Imaging");
-    ADD(dream, "DREAM B1-Mapping");
-    ADD(b1_papp, "B1- estimation via Papp method");
+    args::Group b1(parser, "B1");
+    ADD(afi, b1, "Actual Flip-Angle Imaging");
+    ADD(b1_papp, b1, "B1- estimation via Papp method");
+    ADD(dream, b1, "DREAM B1-Mapping");
 #endif
 #ifdef BUILD_MT
-    ADD(lineshape, "Calculate lineshapes");
-    ADD(lorentzian, "Lorentzian fitting for CEST");
-    ADD(mtr, "Calculate Magnetization Transfer Ratios");
-    ADD(mtsat, "Calculate MTsat");
-    ADD(qmt, "Fit qMT data with Ramani model");
-    ADD(ssfp_emt, "Fit qMT SSFP data with Wood model");
-    ADD(zspec_b1, "B1 correction for Z-spectra");
-    ADD(zspec_interp, "Interpolate Z-spectra (B0 correction etc.)");
+    args::Group mt(parser, "MT");
+    ADD(lineshape, mt, "Calculate lineshapes");
+    ADD(lorentzian, mt, "Lorentzian fitting for CEST");
+    ADD(mtr, mt, "Calculate Magnetization Transfer Ratios");
+    ADD(mtsat, mt, "Calculate MTsat");
+    ADD(qmt, mt, "Fit qMT data with Ramani model");
+    ADD(ssfp_emt, mt, "Fit qMT SSFP data with Wood model");
+    ADD(zspec_b1, mt, "B1 correction for Z-spectra");
+    ADD(zspec_interp, mt, "Interpolate Z-spectra (B0 correction etc.)");
 #endif
 #ifdef BUILD_PERFUSION
-    ADD(ase_oef, "Fit the Blockley model for Oxygen Extraction Fraction");
-    ADD(asl, "Calculate perfusion values with standard ASL model");
-    ADD(zshim, "Perform Z-shimming");
+    args::Group perfusion(parser, "PERFUSION");
+    ADD(ase_oef, perfusion, "Fit the Blockley model for Oxygen Extraction Fraction");
+    ADD(asl, perfusion, "Calculate perfusion values with standard ASL model");
+    ADD(zshim, perfusion, "Perform Z-shimming");
 #endif
 #ifdef BUILD_RELAX
-    ADD(jsr, "Fit Joint System Relaxometry model (T1/T2)")
-    ADD(mpm_r2s, "Fit for R2* across multiple contrasts (ECSTATICS)");
-    ADD(ssfp_ellipse, "Calculuate the SSFP ellipse properties");
-    ADD(despot1, "DESPOT1/VFA methods");
-    ADD(despot1hifi, "DESPOT1-HIFI simultaneous T1/B1 mapping");
-    ADD(despot2, "DESPOT2");
-    ADD(despot2fm, "DESPOT2-FM simultaneous T2/B0 mapping");
-    ADD(mp2rage, "MP2-RAGE estimation of T1");
-    ADD(multiecho, "Multi-echo T2/T2*");
-    ADD(planet, "PLANET method of T1/T2 mapping");
+    args::Group relax(parser, "RELAXOMETRY");
+    ADD(despot1, relax, "DESPOT1/VFA methods");
+    ADD(despot1hifi, relax, "DESPOT1-HIFI simultaneous T1/B1 mapping");
+    ADD(despot2, relax, "DESPOT2");
+    ADD(despot2fm, relax, "DESPOT2-FM simultaneous T2/B0 mapping");
+    ADD(jsr, relax, "Fit Joint System Relaxometry model (T1/T2)")
+    ADD(mpm_r2s, relax, "Fit for R2* across multiple contrasts (ECSTATICS)");
+    ADD(mp2rage, relax, "MP2-RAGE estimation of T1");
+    ADD(multiecho, relax, "Multi-echo T2/T2*");
+    ADD(planet, relax, "PLANET method of T1/T2 mapping");
+    ADD(ssfp_ellipse, relax, "Calculuate the SSFP ellipse properties");
 #endif
 #ifdef BUILD_PARMESAN
-    ADD(transient, "Transient type PARMESAN");
-    ADD(ss, "Steady-state PARMESAN");
-    ADD(rf_sim, "Calculate RF pulse properties for PARMESAN");
+    args::Group parmesan(parser, "PARMESAN");
+    ADD(transient, parmesan, "Transient type PARMESAN");
+    ADD(ss, parmesan, "Steady-state PARMESAN");
+    ADD(rf_sim, parmesan, "Calculate RF pulse properties for PARMESAN");
 #endif
 #ifdef BUILD_STATS
-    ADD(glm_contrasts, "Calculate group means etc. for a GLM");
-    ADD(glm_setup, "Setup files for input to FSL randomise etc.");
-    ADD(rois, "Extract ROI values");
+    args::Group stats(parser, "STATS");
+    ADD(glm_contrasts, stats, "Calculate group means etc. for a GLM");
+    ADD(glm_setup, stats, "Setup files for input to FSL randomise etc.");
+    ADD(rois, stats, "Extract ROI values");
 #endif
 #ifdef BUILD_SUSCEP
-    ADD(fieldmap, "Calculate a B0 map from multi-echo data");
-    ADD(unwrap_laplace, "Laplacian phase unwrapping");
-    ADD(unwrap_path, "Path-based phase unwrapping");
+    args::Group suscep(parser, "SUSCEPTIBILITY");
+    ADD(fieldmap, suscep, "Calculate a B0 map from multi-echo data");
+    ADD(unwrap_laplace, suscep, "Laplacian phase unwrapping");
+    ADD(unwrap_path, suscep, "Path-based phase unwrapping");
 #endif
 #ifdef BUILD_UTILS
-    ADD(affine, "Edit the affine transformation in an image header");
-    ADD(affine_angle, "Calculuate the angle from the Z-axis of the header affine transform");
-    ADD(coil_combine, "Combine images from multi-channel coils");
-    ADD(complex, "Convert real/imaginary/magnitude/phase/complex data");
-    ADD(gradient, "Calculate the gradients of an image");
-    ADD(kfilter, "Filter an image via k-space");
-    ADD(mask, "Calculate a mask using various threshold based methods");
-    ADD(noise_est, "Calculate noise values in a region/mask");
-    ADD(pca, "Perform PCA noise reduction on multi-volume data");
-    ADD(polyfit, "Fit a polynomial to an image");
-    ADD(polyimg, "Create an image from a polynomial");
-    ADD(rfprofile, "Multiply a B1 map by a slab profile");
-    ADD(select, "Choose volumes from a 4D image");
-    ADD(ssfp_bands, "Remove banding artefacts from SSFP images");
-    ADD(tvmask, "Calculate a mask from 4D data using Total-Variation");
+    args::Group utils(parser, "UTILITIES");
+    ADD(affine, utils, "Edit the affine transformation in an image header");
+    ADD(affine_angle, utils, "Calculuate the angle from the Z-axis of the header affine transform");
+    ADD(coil_combine, utils, "Combine images from multi-channel coils");
+    ADD(complex, utils, "Convert real/imaginary/magnitude/phase/complex data");
+    ADD(gradient, utils, "Calculate the gradients of an image");
+    ADD(kfilter, utils, "Filter an image via k-space");
+    ADD(mask, utils, "Calculate a mask using various threshold based methods");
+    ADD(noise_est, utils, "Calculate noise values in a region/mask");
+    ADD(pca, utils, "Perform PCA noise reduction on multi-volume data");
+    ADD(polyfit, utils, "Fit a polynomial to an image");
+    ADD(polyimg, utils, "Create an image from a polynomial");
+    ADD(rfprofile, utils, "Multiply a B1 map by a slab profile");
+    ADD(select, utils, "Choose volumes from a 4D image");
+    ADD(ssfp_bands, utils, "Remove banding artefacts from SSFP images");
+    ADD(tvmask, utils, "Calculate a mask from 4D data using Total-Variation");
 #endif
 #undef ADD
 
