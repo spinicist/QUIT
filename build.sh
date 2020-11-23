da1d7a1fe6d6f6a33e8dfb3485b460a606ad1e2f
spinicist <tobias@spinicist.org.uk>
2020-11-23 21:36:31 +0000

    BLD Bump vcpkg/eigen

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 vcpkg/bootstrap-vcpkg.sh -allowAppleClang
 cmake -B build -S . \
     -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_TARGET_TRIPLET=x64-osx-asan \
+    -DVCPKG_TARGET_TRIPLET=x64-osx-asan -DCMAKE_BUILD_TYPE=Release \
     -GNinja
 cmake --build build
