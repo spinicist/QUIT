fd82d7309a19054d584c5fe82c576329f36d4a4e
spinicist <tobias@spinicist.org.uk>
2021-06-07 11:32:36 +0100

    Provide B1/no B1 MPM workflows

--- Python/qipype/workflows/mpm.py
+++ Python/qipype/workflows/mpm.py
@@ -8,7 +8,7 @@ from qipype.fsl import ApplyXfm4D
 from qipype.utils import Select
 
 
-def init_mpm_wf(me_params, mtsat_params):
+def init_mpm_b1_wf(me_params, mtsat_params):
     inputnode = Node(IdentityInterface(fields=['PDw_file', 'T1w_file', 'MTw_file',
                                                'PDw_cal', 'T1w_cal', 'MTw_cal',
                                                'B1_map']),
@@ -80,6 +80,58 @@ def init_mpm_wf(me_params, mtsat_params):
     mtsat = Node(MTSat(sequence=mtsat_params, verbose=True), name='MPM_MTSat')
 
     wf.connect([(pd_cal, mpm, [('out_file', 'PDw_file')]),
+                (t1_apply, mpm, [('out_file', 'T1w_file')]),
+                (mt_apply, mpm, [('out_file', 'MTw_file')]),
+                (bet, mpm, [('mask_file', 'mask_file')]),
+                (mpm, mtsat, [('S0_PDw_map', 'PDw_file'),
+                              ('S0_T1w_map', 'T1w_file'),
+                              ('S0_MTw_map', 'MTw_file')]),
+                (inputnode, mtsat, [('B1_map', 'B1_map')]),
+                (bet, mtsat, [('mask_file', 'mask_file')]),
+                (mpm, outputnode, [('R2s_map', 'R2s_map')]),
+                (mtsat, outputnode, [('PD_map', 'PD_map'),
+                                     ('R1_map', 'R1_map'),
+                                     ('delta_map', 'mtsat_map')])])
+    return wf
+
+
+def init_mpm_wf(me_params, mtsat_params):
+    inputnode = Node(IdentityInterface(fields=['PDw_file', 'T1w_file', 'MTw_file']),
+                     name='inputnode')
+    outputnode = Node(IdentityInterface(fields=['PD_map', 'R1_map', 'R2s_map', 'mtsat_map']),
+                      name='outputnode')
+
+    wf = Workflow(name='Multi-Parametric-Mapping')
+
+    bet = Node(BET(mask=True, no_output=True), name='brain_mask')
+    wf.connect([(inputnode, bet, [('T1w_file', 'in_file')])])
+
+    pd0 = Node(Select(volumes=[0, ], out_file='pd0.nii.gz'), name='pd0')
+    t10 = Node(Select(volumes=[0, ], out_file='t10.nii.gz'), name='t10')
+    mt0 = Node(Select(volumes=[0, ], out_file='mt0.nii.gz'), name='mt0')
+    wf.connect([(inputnode, pd0, [('PDw_file', 'in_file')]),
+                (inputnode, t10, [('T1w_file', 'in_file')]),
+                (inputnode, mt0, [('MTw_file', 'in_file')])])
+
+    t1_reg = Node(FLIRT(uses_qform=True, cost='mutualinfo'), name='t1_reg')
+    mt_reg = Node(FLIRT(uses_qform=True, cost='mutualinfo'), name='mt_reg')
+    t1_apply = Node(ApplyXfm4D(single_matrix=True), name='t1_apply')
+    mt_apply = Node(ApplyXfm4D(single_matrix=True), name='mt_apply')
+    wf.connect([(t10, t1_reg, [('out_file', 'in_file')]),
+                (pd0, t1_reg, [('out_file', 'reference')]),
+                (inputnode, t1_apply, [('T1w_file', 'in_file')]),
+                (pd0, t1_apply, [('out_file', 'ref_vol')]),
+                (t1_reg, t1_apply, [('out_matrix_file', 'trans_file')]),
+                (mt0, mt_reg, [('out_file', 'in_file')]),
+                (pd0, mt_reg, [('out_file', 'reference')]),
+                (inputnode, mt_apply, [('MTw_file', 'in_file')]),
+                (pd0, mt_apply, [('out_file', 'ref_vol')]),
+                (mt_reg, mt_apply, [('out_matrix_file', 'trans_file')])])
+
+    mpm = Node(MPMR2s(sequence=me_params, verbose=True), name='MPM_R2s')
+    mtsat = Node(MTSat(sequence=mtsat_params, verbose=True), name='MPM_MTSat')
+
+    wf.connect([(inputnode, mpm, [('PDw_file', 'PDw_file')]),
                 (t1_apply, mpm, [('out_file', 'T1w_file')]),
                 (mt_apply, mpm, [('out_file', 'MTw_file')]),
                 (bet, mpm, [('mask_file', 'mask_file')]),
