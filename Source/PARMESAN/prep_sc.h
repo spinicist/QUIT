a354e3cffe7bf5196c09903e35873694d3d57d18
Tobias Wood <tobias@spinicist.org.uk>
2025-05-02 10:42:04 +0100

    Relax bounds

--- Source/PARMESAN/prep_sc.h
+++ Source/PARMESAN/prep_sc.h
@@ -11,8 +11,8 @@ struct PrepModel : QI::Model<double, double, 5, 0, 1, 0, QI::RealNoise<double>> 
     Eigen::MatrixXd const             basis = Eigen::MatrixXd();
     std::array<std::string, NV> const varying_names{"M0", "T1", "T2", "B1", "df"};
     VaryingArray const                start{1., 1., 0.1, 1.0, 0.0};
-    VaryingArray const                lo{0.1, 0.01, 0.01, 0.5, -100.0};
-    VaryingArray const                hi{100., 5.0, 3.5, 1.5, 100.0};
+    VaryingArray const                lo{0.01, 0.01, 0.01, 0.5, -250.0};
+    VaryingArray const                hi{1000., 5.0, 3.5, 1.5, 250.0};
     auto                              input_size(const int /* Unused */) const -> int;
     auto signal(VaryingArray const &v, FixedArray const &) const -> QI_ARRAY(DataType);
 };
