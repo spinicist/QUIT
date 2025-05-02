1705d1989c57db5f03311bb5d44c6dce6cfc772a
Tobias Wood <tobias@spinicist.org.uk>
2025-05-02 22:36:21 +0100

    Fix k full parameter count

--- Source/PARMESAN/prep_qmt.h
+++ Source/PARMESAN/prep_qmt.h
@@ -68,16 +68,16 @@ struct PrepQMTk : Model<double, double, 4, 4, 1, 0, RealNoise<double>> {
     auto signal(VaryingArray const &v, FixedArray const &f) const -> QI_ARRAY(DataType);
 };
 
-struct PrepQMTkFull : Model<double, double, 7, 1, 1, 0, RealNoise<double>> {
+struct PrepQMTkFull : Model<double, double, 9, 1, 1, 0, RealNoise<double>> {
     static int const      NS = 1; // Number of parameters that need to be scaled
     PrepSequence const   &sequence;
     InterpGrid const    &A_sl;
     Eigen::MatrixXd const basis = Eigen::MatrixXd();
     // Pools are "free" and "semi-solid"
-    std::array<std::string, NV> const varying_names{"M0", "T1_f", "T1_s", "T2_s", "f_s", "k", "B1"};
-    VaryingArray const                start{1.0, 2.0, 0.35, 14e-6, 0.1, 1.4, 1.0};
-    VaryingArray const                lo{0.01, 0.5, 0.05, 12e-6, 0.01, 0.1, 0.5};
-    VaryingArray const                hi{100.0, 5.0, 0.5, 20e-6, 0.5, 10.0, 1.5};
+    std::array<std::string, NV> const varying_names{"M0", "T1_f", "T2_f", "T1_s", "T2_s", "f_s", "k", "B1", "df0"};
+    VaryingArray const start{1.0, 2.0, 0.07, 0.35, 14e-6, 0.1, 1.4, 1.0, 0.0};
+    VaryingArray const lo{0.01, 0.5, 0.02, 0.05, 12e-6, 0.01, 0.1, 0.5, -150.0};
+    VaryingArray const hi{100.0, 5.0, 2.5, 0.5, 20e-6, 0.5, 10.0, 1.5, 150.0};
 
     std::array<std::string, NF> const fixed_names{"T2_f"};
     FixedArray const                  fixed_defaults{0.1};
